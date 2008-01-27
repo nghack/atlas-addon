cf20fa530acb9d72e5ab9c2f18eec155a34ec821
diablohudream <diablohudream@23c0fd96-f639-0410-9050-cdd92df7801c>
2008-01-27 08:07:16 +0000

    zhCN update

--- trunk/Atlas_Battlegrounds/Locale/AtlasBattlegrounds-zhCN.lua
+++ trunk/Atlas_Battlegrounds/Locale/AtlasBattlegrounds-zhCN.lua
@@ -3,7 +3,7 @@
 -- Atlas Localization Data (Chinese)
 -- Initial translation by DiabloHu
 -- Version : Chinese (by DiabloHu)
--- Last Update : 1/25/2008
+-- Last Update : 1/27/2008
 -- http://www.dreamgen.cn
 
 --]]
@@ -22,43 +22,44 @@ if ( GetLocale() == "zhCN" ) then
 
 	local myData = {
 		AlteracValleyNorth = {
-			ZoneName = "奥特兰克山谷 (北部)";
+			ZoneName = "奥特兰克山谷 (北部, 联盟)";
+			Acronym = "AV";
 			Location = "奥特兰克山脉";
 			ORNG.."阵营：霜狼氏族 (部落)";
 			ORNG.."阵营：雷矛卫队 (联盟)";
 			BLUE.."A) 入口";
-			BLUE.."B) 丹巴达尔 (联盟)";
-			BLUE..INDENT.."范达尔·雷矛 (联盟)";
-			BLUE..INDENT.."丹巴达尔北部统帅 (联盟)";
-			BLUE..INDENT.."丹巴达尔南部统帅 (联盟)";
-			BLUE..INDENT.."冰翼统帅 (联盟)";
-			BLUE..INDENT.."石炉统帅 (联盟)";
-			BLUE..INDENT.."勘查员塔雷·石镐 (联盟)";
+			BLUE.."B) 丹巴达尔";
+			BLUE..INDENT.."范达尔·雷矛";
+			BLUE..INDENT.."丹巴达尔北部统帅";
+			BLUE..INDENT.."丹巴达尔南部统帅";
+			BLUE..INDENT.."冰翼统帅";
+			BLUE..INDENT.."石炉统帅";
+			BLUE..INDENT.."勘查员塔雷·石镐";
 			_RED.."1) 深铁矿洞";
 			GREY..INDENT.."莫洛克 (中立)";
-			GREY..INDENT.."乌米·索尔森 (联盟)";
+			GREY..INDENT.."乌米·索尔森";
 			GREY..INDENT.."基塔尔 (部落)";
-			GREY.."2) 大德鲁伊雷弗拉尔 (联盟)";
+			GREY.."2) 大德鲁伊雷弗拉尔";
 			ORNG.."3) 丹巴达尔北部碉堡";
 			GREY..INDENT.."空军指挥官穆维里克 (部落)";
-			GREY.."4) 莫高特·深炉 (联盟)";
-			GREY..INDENT.."德尔克 <赏金猎人> (联盟)";
-			GREY..INDENT.."亚斯拉玛尼斯 <赏金猎人> (联盟)";
-			GREY..INDENT.."兰纳·雷酒 <锻造供应商> (联盟)";
+			GREY.."4) 莫高特·深炉";
+			GREY..INDENT.."德尔克 <赏金猎人>";
+			GREY..INDENT.."亚斯拉玛尼斯 <赏金猎人>";
+			GREY..INDENT.."兰纳·雷酒 <锻造供应商>";
 			_RED.."5) 雷矛急救站";
-			GREY.."6) 雷矛兽栏管理员 <兽栏管理员> (联盟)";
-			GREY..INDENT.."雷矛山羊骑兵指挥官 (联盟)";
-			GREY..INDENT.."斯瓦尔布莱德·远山 <商人> (联盟)";
-			GREY..INDENT.."库德拉姆·麦须 <毒药和材料> (联盟)";
-			GREY.."7) 雷矛军需官 (联盟)";
-			GREY..INDENT.."约尼维拉·远山 <杂货商> (联盟)";
-			GREY..INDENT.."布罗古斯·雷酒 <食物和饮料> (联盟)";
+			GREY.."6) 雷矛兽栏管理员 <兽栏管理员>";
+			GREY..INDENT.."雷矛山羊骑兵指挥官";
+			GREY..INDENT.."斯瓦尔布莱德·远山 <商人>";
+			GREY..INDENT.."库德拉姆·麦须 <毒药和材料>";
+			GREY.."7) 雷矛军需官";
+			GREY..INDENT.."约尼维拉·远山 <杂货商>";
+			GREY..INDENT.."布罗古斯·雷酒 <食物和饮料>";
 			GREY.."8) 空军指挥官艾克曼 (被营救)";
 			GREY..INDENT.."空军指挥官斯里多尔 (被营救)";
 			GREY..INDENT.."空军指挥官维波里 (被营救)";
 			ORNG.."9) 丹巴达尔南部碉堡";
-			GREY..INDENT.."诺雷格·雷矛中尉 (联盟)";
-			GREY..INDENT.."盖尔丁 <雷矛军需官> (联盟)";
+			GREY..INDENT.."诺雷格·雷矛中尉";
+			GREY..INDENT.."盖尔丁 <雷矛军需官>";
 			_RED.."10) 雷矛墓地";
 			GREY.."11) 冰翼洞穴";
 			GREY..INDENT.."雷矛军旗";
@@ -67,19 +68,19 @@ if ( GetLocale() == "zhCN" ) then
 			ORNG.."13) 冰翼碉堡";
 			GREY..INDENT.."空军指挥官古斯 (部落)";
 			_RED.."14) 石炉墓地";
-			GREY.."15) 雷矛山羊骑兵指挥官 (联盟)";
+			GREY.."15) 雷矛山羊骑兵指挥官";
 			ORNG.."16) 石炉哨站";
-			GREY..INDENT.."巴琳达·斯通赫尔斯 <雷矛上尉> (联盟)";
+			GREY..INDENT.."巴琳达·斯通赫尔斯 <雷矛上尉>";
 			_RED.."17) 雪落墓地";
-			GREY..INDENT.."艾克曼的信号灯 (联盟)";
+			GREY..INDENT.."艾克曼的信号灯";
 			GREY..INDENT.."穆维里克的信号灯 (部落)";
 			ORNG.."18) 石炉碉堡";
-			GREY.."19) 森林之王伊弗斯 (召唤区域) (联盟)";
+			GREY.."19) 森林之王伊弗斯 (召唤区域)";
 			GREY.."20) 西部平原";
-			GREY..INDENT.."维波里的信号灯 (联盟)";
+			GREY..INDENT.."维波里的信号灯";
 			GREY..INDENT.."杰斯托的信号灯 (部落)";
 			GREY.."21) 东部平原";
-			GREY..INDENT.."斯里多尔的信号灯 (联盟)";
+			GREY..INDENT.."斯里多尔的信号灯";
 			GREY..INDENT.."古斯的信号灯 (部落)";
 			"";
 			_RED.."红色:"..BLUE.." 墓地, 占领区域";
@@ -87,12 +88,13 @@ if ( GetLocale() == "zhCN" ) then
 			GREY.."白色:"..BLUE.." 相关 NPC, 任务区域";
 		};
 		AlteracValleySouth = {
-			ZoneName = "奥特兰克山谷 (南部)";
+			ZoneName = "奥特兰克山谷 (南部, 部落)";
+			Acronym = "AV";
 			Location = "奥特兰克山脉";
 			ORNG.."阵营：霜狼氏族 (部落)";
 			ORNG.."阵营：雷矛卫队 (联盟)";
 			BLUE.."A) 入口";
-			BLUE.."B) 霜狼要塞 (部落)";
+			BLUE.."B) 霜狼要塞";
 			_RED.."1) 霜狼急救站";
 			_RED.."2) 霜狼墓地";
 			_RED.."3) 冰血墓地";
@@ -101,7 +103,7 @@ if ( GetLocale() == "zhCN" ) then
 			GREY..INDENT.."空军指挥官艾克曼 (联盟)";
 			ORNG.."6) 哨塔高地";
 			GREY..INDENT.."空军指挥官斯里多尔 (联盟)";
-			GREY..INDENT.."指挥官刘易斯·菲利普 (部落)";
+			GREY..INDENT.."指挥官刘易斯·菲利普";
 			ORNG.."7) 冰血哨塔";
 			ORNG.."8) 冰血要塞 (加尔范)";
 			GREY.."9) 蛮爪洞穴";
@@ -117,6 +119,7 @@ if ( GetLocale() == "zhCN" ) then
 		};
 		ArathiBasin = {
 			ZoneName = "阿拉希盆地";
+			Acronym = "AB";
 			Location = "阿拉希高地";
 			ORNG.."阵营：污染者 (部落)";
 			ORNG.."阵营：阿拉索联军 (联盟)";
@@ -130,6 +133,7 @@ if ( GetLocale() == "zhCN" ) then
 		};
 		WarsongGulch = {
 			ZoneName = "战歌峡谷";
+			Acronym = "WSG";
 			Location = "灰谷/贫瘠之地";
 			ORNG.."阵营：战歌侦察骑兵 (部落)";
 			ORNG.."阵营：银翼要塞 (联盟)";
@@ -138,6 +142,7 @@ if ( GetLocale() == "zhCN" ) then
 		};
 		EyeOfTheStorm = {
 			ZoneName = "风暴之眼";
+			Acronym = "EotS";
 			Location = "虚空风暴";
 			BLUE.."A) 入口 (联盟)";
 			BLUE.."B) 入口 (部落)";

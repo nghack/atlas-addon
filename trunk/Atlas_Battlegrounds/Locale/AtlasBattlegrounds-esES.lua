e4caebf47ddb58e9d2e516793c5fca83a3208c4e
maqjav <maqjav@23c0fd96-f639-0410-9050-cdd92df7801c>
2008-03-24 19:44:24 +0000

    -- esES update (i forgot to send this file)

--- trunk/Atlas_Battlegrounds/Locale/AtlasBattlegrounds-esES.lua
+++ trunk/Atlas_Battlegrounds/Locale/AtlasBattlegrounds-esES.lua
@@ -113,26 +113,41 @@ if ( GetLocale() == "esES" ) then
 			Location = "Laderas de Trabalomas";
 			Acronym = "VA";
 			ORNG.."Reputación: Clan Lobo Gélido (Horda)";
-			ORNG.."Reputación: Guardia Pico Tormenta (Alianza)";
-			BLUE.."A) Entrada";
+			BLUE.."A) Entrada de la horda";
 			BLUE.."B) Bastión Lobo Gélido"; --Bien
-			_RED.."1) Puesto de auxilio de Lobo Gélido"; --Bien
-			_RED.."2) Cementerio Lobo Gélido"; --Bien
-			_RED.."3) Cementerio Sangre Fría"; --Bien
-			ORNG.."4) Torre Lobo Gélido Oeste"; --Bien
-			ORNG.."5) Torre Lobo Gélido Este"; --Bien
-			GREY..INDENT.."Comandante del aire Ichman (Alianza)"; --Bien
-			ORNG.."6) Punto Torre"; --Bien
-			GREY..INDENT.."Comandante del aire Slidore (Alianza)"; --Bien
-			GREY..INDENT.."Comandante Luis Filis"; --Bien
-			ORNG.."7) Torre Sangre Fría"; --Bien
-			ORNG.."8) Baluarte Sangre Fría (Galvangar)"; --Bien
-			GREY.."9) Caverna Zarpa Salvaje"; --Bien
-			GREY.."10) Comandante jinete de lobos Lobo Gélido"; --Bien
-			GREY.."11) Comandante de aire Vipore (Alianza)"; --Bien
-			GREY.."12) Mina Dentefrío"; --Bien
-			GREY.."13) Sierra de vapor (Alianza)"; --Bien
-			GREY.."14) Lokholar el Señor de Hielo (Zona de invocar)"; --Bien
+			BLUE..INDENT.."Drek'Thar"; --Bien
+			BLUE..INDENT.."Duros"; --Bien
+			BLUE..INDENT.."Drakan"; --Bien
+			BLUE..INDENT.."Maestro de guerra del oeste Lobo Gélido"; --Bien
+			BLUE..INDENT.."Maestro de guerra del este Lobo Gélido"; --Bien
+			BLUE..INDENT.."Maestro de guerra de Punta de la Torre"; --Bien
+			BLUE..INDENT.."Maestro de guerra Sangrehielo"; --Bien
+			GREY.."1) Lokholar el Señor de Hielo (Zona de invocar)"; --Bien
+			ORNG.."2) Garrison Sangre Fría"; --Bien
+			GREY..INDENT.."Capitán Galvangar"; --Bien
+			ORNG.."3) Torre Sangre Fría"; --Bien
+			_RED.."4) Torre Lobo Gélido Este"; --Bien
+			GREY.."Comandante del aire Ichman (Alianza)"; --Bien
+			ORNG.."5) Punto Torre";	 --Bien
+			GREY..INDENT.."Comandante del aire Slidore (Alianza)";  --Bien		
+			GREY.."6) Mina Dentefrío"; --Bien
+			GREY..INDENT.."Sniwle (Neutral)"; --Bien
+			GREY..INDENT.."Masha Corteveloz"; --Bien
+			GREY..INDENT.."Aggi Rumblestomp (Alianza)";	 --FALTA	
+			_RED.."7) Torre Lobo Gélido Oeste"; --Bien
+			GREY.."8) Comandante de aire Vipore (Alianza)";	 --Bien
+			GREY..INDENT.."Jotek"; --Bien
+			GREY..INDENT.."Herrero Regzar"; --Bien
+			GREY..INDENT.."Primalist Thurloga"; --Bien
+			GREY..INDENT.."Sargento Yazra Gruñidosangriento"; --Bien
+			GREY.."9) Comandante jinete de lobos Lobo Gélido"; --Bien
+			GREY.."10) Intendente Lobo Gélido"; --Bien
+			GREY.."11) Comandante del aire Guse (Rescatado)";	 --Bien
+			GREY..INDENT.."Comandante del aire Jeztor (Rescatado)"; --Bien
+			GREY..INDENT.."Comandante del aire Mulverick (Rescatado)"; --Bien
+			_RED.."12) Frostwolf Releif Hut"; --FALTA
+			GREY.."13) Caverna Zarpa Salvaje"; --Bien
+			GREY..INDENT.."Estandarte de Lobo Gélido"; --Bien			
 			"";
 			_RED.."Rojo:".._RED.." Cementerios, Areas capturables";
 			ORNG.."Naranja:"..ORNG.." Búnkers, Torres, Areas destruibles";

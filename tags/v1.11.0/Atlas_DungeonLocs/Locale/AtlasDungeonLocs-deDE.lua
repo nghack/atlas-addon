10af52c09422e7a9c598b245a719c2f3c1d134f6
loglow@gmail.com <loglow@gmail.com@23c0fd96-f639-0410-9050-cdd92df7801c>
2008-03-25 17:41:14 +0000

    proper tag copy

---
+++ tags/v1.11.0/Atlas_DungeonLocs/Locale/AtlasDungeonLocs-deDE.lua
@@ -0,0 +1,126 @@
+--[[
+
+	Atlas, a World of Warcraft instance map browser
+	Copyright 2005 - 2008 Dan Gilbert
+	Email me at loglow@gmail.com
+
+	This file is part of Atlas.
+
+	Atlas is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Atlas is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Atlas; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+--]]
+
+if ( GetLocale() == "deDE" ) then
+
+	local BLUE = "|cff6666ff";
+	local GREY = "|cff999999";
+	local GREN = "|cff66cc33";
+	local _RED = "|cffcc6666";
+	local ORNG = "|cffcc9933";
+	local PURP = "|cff9900ff";
+	local INDENT = "      ";
+
+	local myCategory = "Instanzen-Standorte";
+
+	local myData = {
+		DLEast = {
+			ZoneName = "Instanzen-Standorte [A] (Osten)";
+			Location = "Östliche Königreiche";
+			BLUE.."A) Alteractal, ".._RED.."Alteracgebirge / Vorgebirge des Hügellands";
+			BLUE.."B) Arathibecken, ".._RED.."Arathihochland";
+			GREY.."1) Terrasse der Magister, ".._RED.."Insel von Quel'Danas";
+			GREY..INDENT.."Sonnenbrunnenplateau, ".._RED.."Insel von Quel'Danas";
+			GREY.."2) Zul'Aman, ".._RED.."Geisterlande";
+			GREY.."3) Das scharlachrote Kloster, ".._RED.."Tirisfal";
+			GREY.."4) Stratholme, ".._RED.."Östliche Pestländer";
+			GREY..INDENT.."Naxxramas, ".._RED.."Pestwald, Östliche Pestländer";
+			GREY.."5) Scholomance, ".._RED.."Westliche Pestländer";
+			GREY.."6) Burg Schattenfang, ".._RED.."Silberwald";
+			GREY.."7) Gnomeregan, ".._RED.."Dun Morogh";
+			GREY.."8) Uldaman, ".._RED.."Ödland";
+			GREY.."9) Pechschwingenhort, ".._RED.."Schwarzfelsspitze";
+			GREY..INDENT.."Schwarzfelstiefen, ".._RED.."Der Schwarzfels";
+			GREY..INDENT.."Schwarzfelsspitze, ".._RED.."Der Schwarzfels";
+			GREY..INDENT.."Geschmolzener Kern, ".._RED.."Schwarzfelstiefen";
+			GREY.."10) Das Verlies, ".._RED.."Sturmwind";
+			GREY.."11) Die Todesminen, ".._RED.."Westfall";
+			GREY.."12) Zul'Gurub, ".._RED.."Schlingendorntal";
+			GREY.."13) Karazhan, ".._RED.."Gebirgspass der Totenwinde";
+			GREY.."14) Der Tempel von Atal'Hakkar, ".._RED.."Sümpfe des Elends";
+			"";
+			"";
+			BLUE.."Blau:"..ORNG.." Schlachtfelder";
+			GREY.."Weiß:"..ORNG.." Instanzen";
+		};
+		DLWest = {
+			ZoneName = "Instanzen-Standorte [B] (Westen)";
+			Location = "Kalimdor";
+			BLUE.."A) Kriegshymnenschlucht, ".._RED.."Brachland / Eschental";
+			GREY.."1) Tiefschwarze Grotte, ".._RED.."Eschental";
+			GREY.."2) Der Flammenschlund, ".._RED.."Orgrimmar";
+			GREY.."3) Die Höhlen des Wehklagens, ".._RED.."Brachland";
+			GREY.."4) Maraudon, ".._RED.."Desolace";
+			GREY.."5) Düsterbruch, ".._RED.."Feralas";
+			GREY.."6) Kral der Klingenhauer, ".._RED.."Brachland";
+			GREY.."7) Hügel der Klingenhauer, ".._RED.."Brachland";
+			GREY.."8) Onyxias Hort, ".._RED.."Düstermarschen";
+			GREY.."9) Zul'Farrak, ".._RED.."Tanaris";
+			GREY.."10) Höhlen der Zeit, ".._RED.."Tanaris";
+			GREY..INDENT.."Vorgebirge des Alten Hügellands";
+			GREY..INDENT.."Der schwarze Morast";
+			GREY..INDENT.."Hyjalgipfel";
+			GREY.."11) Ruinen von Ahn'Qiraj, ".._RED.."Silithus";
+			GREY..INDENT.."Tempel von Ahn'Qiraj, ".._RED.."Silithus";
+			"";
+			"";
+			"";
+			"";
+			"";
+			"";
+			"";
+			BLUE.."Blau:"..ORNG.." Schlachtfelder";
+			GREY.."Weiß:"..ORNG.." Instanzen";
+		};
+		DLOutland = {
+			ZoneName = "Instanzen-Standorte [C] (Scherbenwelt)";
+			Location = "Scherbenwelt";
+			GREY.."1) Gruuls Unterschlupf, ".._RED.."Schergrat";
+			GREY.."2) Festung der Stürme, ".._RED.."Nethersturm";
+			GREY..INDENT.."Die Mechanar";
+			GREY..INDENT.."Die Botanika";
+			GREY..INDENT.."Die Arkatraz";
+			GREY..INDENT.."Festung der Stürme";
+			GREY.."3) Der Echsenkessel, ".._RED.."Zangarmarschen";
+			GREY..INDENT.."Die Sklavenunterkünfte";
+			GREY..INDENT.."Der Tiefensumpf";
+			GREY..INDENT.."Die Dampfkammer";
+			GREY..INDENT.."Höhle des Schlangenschreins";
+			GREY.."4) Höllenfeuerzitadelle, ".._RED.."Höllenfeuerhalbinsel";
+			GREY..INDENT.."Höllenfeuerbollwerk";
+			GREY..INDENT.."Der Blutkessel";
+			GREY..INDENT.."Die zerschmetterten Hallen";
+			GREY..INDENT.."Magtheridons Kammer";
+			GREY.."5) Auchindoun, ".._RED.."Wälder von Terrokar";
+			GREY..INDENT.."Managruft";
+			GREY..INDENT.."Auchenaikrypta";
+			GREY..INDENT.."Sethekkhallen";
+			GREY..INDENT.."Schattenlabyrinth"; 
+			GREY.."6) Der Schwarze Tempel, ".._RED.."Schattenmondtal";
+		};
+	};
+
+	Atlas_RegisterPlugin("Atlas_DungeonLocs", myCategory, myData);
+
+end
---
+++ tags/v1.11.0/Atlas_DungeonLocs/Locale/AtlasDungeonLocs-zhCN.lua
@@ -0,0 +1,112 @@
+﻿--[[
+
+-- Atlas Localization Data (Chinese)
+-- Initial translation by DiabloHu
+-- Version : Chinese (by DiabloHu)
+-- Last Update : 3/10/2008
+-- http://www.dreamgen.cn
+
+--]]
+
+if ( GetLocale() == "zhCN" ) then
+
+	local BLUE = "|cff6666ff";
+	local GREY = "|cff999999";
+	local GREN = "|cff66cc33";
+	local _RED = "|cffcc6666";
+	local ORNG = "|cffcc9933";
+	local PURP = "|cff9900ff";
+	local INDENT = "　";
+
+	local myCategory = "副本分布图";
+
+	local myData = {
+		DLEast = {
+			ZoneName = "地下城分布图 [A] (东部)";
+			Location = "东部王国";
+			BLUE.."A) 奥特兰克山谷, ".._RED.."奥特兰克山脉";
+			BLUE.."B) 阿拉希盆地, ".._RED.."阿拉希高地";
+			GREY.."1) 魔导师平台, ".._RED.."奎尔丹纳斯岛";
+			GREY..INDENT.."太阳之井高地, ".._RED.."奎尔丹纳斯岛";
+			GREY.."2) 祖阿曼, ".._RED.."幽魂之地";
+			GREY.."3) 血色修道院, ".._RED.."提瑞斯法林地";
+			GREY.."4) 斯坦所姆, ".._RED.."东瘟疫之地";
+			GREY..INDENT.."纳克萨玛斯, ".._RED.."斯坦所姆";
+			GREY.."5) 通灵学院, ".._RED.."西瘟疫之地";
+			GREY.."6) 影牙城堡, ".._RED.."银松森林";
+			GREY.."7) 诺莫瑞根, ".._RED.."丹莫罗";
+			GREY.."8) 奥达曼, ".._RED.."荒芜之地";
+			GREY.."9) 黑翼之巢, ".._RED.."黑石塔";
+			GREY..INDENT.."黑石深渊, ".._RED.."黑石山";
+			GREY..INDENT.."黑石塔, ".._RED.."黑石山";
+			GREY..INDENT.."熔火之心, ".._RED.."黑石深渊";
+			GREY.."10) 监狱, ".._RED.."暴风城";
+			GREY.."11) 死亡矿井, ".._RED.."西部荒野";
+			GREY.."12) 祖尔格拉布, ".._RED.."荆棘谷";
+			GREY.."13) 卡拉赞, ".._RED.."逆风小径";
+			GREY.."14) 沉没的神庙, ".._RED.."悲伤沼泽";
+			"";
+			"";
+			BLUE.."蓝色:"..ORNG.." 战场";
+			GREY.."白色:"..ORNG.." 副本地下城";
+		};
+		DLWest = {
+			ZoneName = "地下城分布图 [B] (西部)";
+			Location = "卡利姆多";
+			BLUE.."A) 战歌峡谷, ".._RED.."贫瘠之地/灰谷";
+			GREY.."1) 黑暗深渊, ".._RED.."灰谷";
+			GREY.."2) 怒焰裂谷, ".._RED.."奥格瑞玛";
+			GREY.."3) 哀嚎洞穴, ".._RED.."贫瘠之地";
+			GREY.."4) 玛拉顿, ".._RED.."凄凉之地";
+			GREY.."5) 厄运之槌, ".._RED.."菲拉斯";
+			GREY.."6) 剃刀沼泽, ".._RED.."贫瘠之地";
+			GREY.."7) 剃刀高地, ".._RED.."贫瘠之地";
+			GREY.."8) 奥妮克希亚的巢穴, ".._RED.."尘泥沼泽";
+			GREY.."9) 祖尔法拉克, ".._RED.."塔纳利斯";
+			GREY.."10) 时光之穴, ".._RED.."塔纳利斯";
+			GREY..INDENT.."旧希尔斯布莱德丘陵";
+			GREY..INDENT.."黑色沼泽";
+			GREY..INDENT.."海加尔山";
+			GREY.."11) 安其拉废墟, ".._RED.."希利苏斯";
+			GREY..INDENT.."安其拉神殿, ".._RED.."希利苏斯";
+			"";
+			"";
+			"";
+			"";
+			"";
+			"";
+			"";
+			BLUE.."蓝色:"..ORNG.." 战场";
+			GREY.."白色:"..ORNG.." 副本地下城";
+		};
+		DLOutland = {
+			ZoneName = "地下城分布图 [C] (外域)";
+			Location = "外域";
+			GREY.."1) 格鲁尔的巢穴, ".._RED.."刀锋山";
+			GREY.."2) 风暴要塞, ".._RED.."虚空风暴";
+			GREY..INDENT.."能量舰";
+			GREY..INDENT.."生态船";
+			GREY..INDENT.."禁魔监狱";
+			GREY..INDENT.."风暴之眼";
+			GREY.."3) 盘牙水库, ".._RED.."赞加沼泽";
+			GREY..INDENT.."奴隶围栏";
+			GREY..INDENT.."幽暗沼泽";
+			GREY..INDENT.."蒸汽地窖";
+			GREY..INDENT.."毒蛇神殿";
+			GREY.."4) 地狱火堡垒, ".._RED.."地狱火半岛";
+			GREY..INDENT.."地狱火城墙";
+			GREY..INDENT.."鲜血熔炉";
+			GREY..INDENT.."碎裂大厅";
+			GREY..INDENT.."玛瑟里顿的巢穴";
+			GREY.."5) 奥金顿, ".._RED.."泰罗卡森林";
+			GREY..INDENT.."法力陵墓";
+			GREY..INDENT.."奥金尼地穴";
+			GREY..INDENT.."塞泰克大厅";
+			GREY..INDENT.."暗影迷宫";
+			GREY.."6) 黑暗神殿, ".._RED.."影月谷";
+		};
+	};
+
+	Atlas_RegisterPlugin("Atlas_DungeonLocs", myCategory, myData);
+
+end

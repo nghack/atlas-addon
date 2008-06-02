e7fab070575aa3a11c450b8d5992b62ca3ed5ca9
loglow@gmail.com <loglow@gmail.com@23c0fd96-f639-0410-9050-cdd92df7801c>
2008-06-02 21:12:18 +0000

    Atlas_Flightpaths converted for localization, GUIDs coming soon.

---
+++ trunk/Atlas_FlightPaths/Locale/Atlas_FlightPaths-enUS.lua
@@ -0,0 +1,187 @@
+--[[
+
+	Atlas, a World of Warcraft instance map browser
+	Copyright 2005 - 2008 Dan Gilbert
+	Email me at loglow@gmail.com
+
+	This file is part of Atlas.
+
+	Atlas is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Atlas is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Atlas; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+--]]
+
+AtlasFPLocale = {
+
+	["Aerie Peak"] = "Aerie Peak";
+	["Aldor"] = "Aldor";
+	["Allerian Stronghold"] = "Allerian Stronghold";
+	["Alliance"] = "Alliance";
+	["Altar of Sha'tar"] = "Altar of Sha'tar";
+	["Arathi Highlands"] = "Arathi Highlands";
+	["Area 52"] = "Area 52";
+	["Ashenvale Forest"] = "Ashenvale Forest";
+	["Ashenvale"] = "Ashenvale";
+	["Astranaar"] = "Astranaar";
+	["Auberdine"] = "Auberdine";
+	["Azshara"] = "Azshara";
+	["Azuremyst Isle"] = "Azuremyst Isle";
+	["Badlands"] = "Badlands";
+	["Blackwind Landing"] = "Blackwind Landing";
+	["Blade's Edge Mountains"] = "Blade's Edge Mountains";
+	["Blood Watch"] = "Blood Watch";
+	["Bloodmyst Isle"] = "Bloodmyst Isle";
+	["Bloodvenom Post"] = "Bloodvenom Post";
+	["Booty Bay"] = "Booty Bay";
+	["Brackenwall Village"] = "Brackenwall Village";
+	["Burning Steppes"] = "Burning Steppes";
+	["Camp Mojache"] = "Camp Mojache";
+	["Camp Taurajo"] = "Camp Taurajo";
+	["Cenarion Hold"] = "Cenarion Hold";
+	["Chillwind Post"] = "Chillwind Post";
+	["Cosmowrench"] = "Cosmowrench";
+	["Crossroads"] = "Crossroads";
+	["Darkshire"] = "Darkshire";
+	["Darkshore"] = "Darkshore";
+	["Desolace"] = "Desolace";
+	["Druid-only"] = "Druid-only";
+	["Dun Morogh"] = "Dun Morogh";
+	["Durotar"] = "Durotar";
+	["Duskwood"] = "Duskwood";
+	["Dustwallow Marsh"] = "Dustwallow Marsh";
+	["East"] = "East";
+	["Eastern Kingdoms"] = "Eastern Kingdoms";
+	["Eastern Plaguelands"] = "Eastern Plaguelands";
+	["Elwynn Forest"] = "Elwynn Forest";
+	["Emerald Sanctuary"] = "Emerald Sanctuary";
+	["Evergrove"] = "Evergrove";
+	["Everlook"] = "Everlook";
+	["Eversong Woods"] = "Eversong Woods";
+	["Falcon Watch"] = "Falcon Watch";
+	["Feathermoon Stronghold"] = "Feathermoon Stronghold";
+	["Felwood"] = "Felwood";
+	["Feralas"] = "Feralas";
+	["Flame Crest"] = "Flame Crest";
+	["Flight Path Maps"] = "Flight Path Maps";
+	["Forest Song"] = "Forest Song";
+	["Freewind Post"] = "Freewind Post";
+	["Gadgetzan"] = "Gadgetzan";
+	["Garadar"] = "Garadar";
+	["Ghostlands"] = "Ghostlands";
+	["Grom'Gol Base Camp"] = "Grom'Gol Base Camp";
+	["Hammerfall"] = "Hammerfall";
+	["Hatchet Hills"] = "Hatchet Hills";
+	["Hellfire Peninsula"] = "Hellfire Peninsula";
+	["Hillsbrad Foothills"] = "Hillsbrad Foothills";
+	["Honor Hold"] = "Honor Hold";
+	["Horde"] = "Horde";
+	["Ironforge"] = "Ironforge";
+	["Isle of Quel'Danas"] = "Isle of Quel'Danas";
+	["Kalimdor"] = "Kalimdor";
+	["Kargath"] = "Kargath";
+	["Lakeshire"] = "Lakeshire";
+	["Light's Hope Chapel"] = "Light's Hope Chapel";
+	["Loch Modan"] = "Loch Modan";
+	["Marshall's Refuge"] = "Marshall's Refuge";
+	["Menethil Harbor"] = "Menethil Harbor";
+	["Mok'Nathal Village"] = "Mok'Nathal Village";
+	["Moonglade"] = "Moonglade";
+	["Morgan's Vigil"] = "Morgan's Vigil";
+	["Mudsprocket"] = "Mudsprocket";
+	["Mulgore"] = "Mulgore";
+	["Nagrand"] = "Nagrand";
+	["Netherguard Keep"] = "Netherguard Keep";
+	["Netherstorm"] = "Netherstorm";
+	["Nighthaven"] = "Nighthaven";
+	["Nijel's Point"] = "Nijel's Point";
+	["Orebor Harborage"] = "Orebor Harborage";
+	["Orgrimmar"] = "Orgrimmar";
+	["Outland"] = "Outland";
+	["Ratchet"] = "Ratchet";
+	["Rebel Camp"] = "Rebel Camp";
+	["Redridge Mountains"] = "Redridge Mountains";
+	["Refuge Point"] = "Refuge Point";
+	["Requires honored faction with Sha'tari Skyguard"] = "Requires honored faction with Sha'tari Skyguard";
+	["Revantusk Village"] = "Revantusk Village";
+	["Rut'Theran Village"] = "Rut'Theran Village";
+	["Sanctum of the Stars"] = "Sanctum of the Stars";
+	["Scryer"] = "Scryer";
+	["Searing Gorge"] = "Searing Gorge";
+	["Sentinel Hill"] = "Sentinel Hill";
+	["Shadowmoon Valley"] = "Shadowmoon Valley";
+	["Shadowmoon Village"] = "Shadowmoon Village";
+	["Shadowprey Village"] = "Shadowprey Village";
+	["Shatter Point"] = "Shatter Point";
+	["Shattrath City"] = "Shattrath City";
+	["Silithus"] = "Silithus";
+	["Silvermoon"] = "Silvermoon";
+	["Silverpine Forest"] = "Silverpine Forest";
+	["Skyguard Outpost"] = "Skyguard Outpost";
+	["South of the path along Lake Elune'ara"] = "South of the path along Lake Elune'ara";
+	["Southshore"] = "Southshore";
+	["Spinebreaker Post"] = "Spinebreaker Post";
+	["Splintertree Post"] = "Splintertree Post";
+	["Stonard"] = "Stonard";
+	["Stonebreaker Hold"] = "Stonebreaker Hold";
+	["Stonetalon Mountains"] = "Stonetalon Mountains";
+	["Stonetalon Peak"] = "Stonetalon Peak";
+	["Stormwind"] = "Stormwind";
+	["Stranglethorn Vale"] = "Stranglethorn Vale";
+	["Sun's Reach"] = "Sun's Reach";
+	["Sunrock Retreat"] = "Sunrock Retreat";
+	["Swamp of Sorrows"] = "Swamp of Sorrows";
+	["Swamprat Post"] = "Swamprat Post";
+	["Sylvanaar"] = "Sylvanaar";
+	["Talonbranch Glade"] = "Talonbranch Glade";
+	["Talrendis Point"] = "Talrendis Point";
+	["Tanaris Desert"] = "Tanaris Desert";
+	["Tarren Mill"] = "Tarren Mill";
+	["Telaar"] = "Telaar";
+	["Teldrassil"] = "Teldrassil";
+	["Telredor"] = "Telredor";
+	["Temple of Telhamat"] = "Temple of Telhamat";
+	["Terokkar Forest"] = "Terokkar Forest";
+	["Thalanaar"] = "Thalanaar";
+	["The Barrens"] = "The Barrens";
+	["The Blasted Lands"] = "The Blasted Lands";
+	["The Dark Portal"] = "The Dark Portal";
+	["The Exodar"] = "The Exodar";
+	["The Hinterlands"] = "The Hinterlands";
+	["The Sepulcher"] = "The Sepulcher";
+	["The Stormspire"] = "The Stormspire";
+	["Thelsamar"] = "Thelsamar";
+	["Theramore Isle"] = "Theramore Isle";
+	["Thorium Point"] = "Thorium Point";
+	["Thousand Needles"] = "Thousand Needles";
+	["Thrallmar"] = "Thrallmar";
+	["Thunder Bluff"] = "Thunder Bluff";
+	["Thunderlord Stronghold"] = "Thunderlord Stronghold";
+	["Tirisfal Glade"] = "Tirisfal Glade";
+	["Toshley's Station"] = "Toshley's Station";
+	["Tranquillien"] = "Tranquillien";
+	["Un'Goro Crater"] = "Un'Goro Crater";
+	["Undercity"] = "Undercity";
+	["Valormok"] = "Valormok";
+	["West of the path to Timbermaw Hold"] = "West of the path to Timbermaw Hold";
+	["West"] = "West";
+	["Western Plaguelands"] = "Western Plaguelands";
+	["Westfall"] = "Westfall";
+	["Wetlands"] = "Wetlands";
+	["Wildhammer Stronghold"] = "Wildhammer Stronghold";
+	["Winterspring"] = "Winterspring";
+	["Zabra'jin"] = "Zabra'jin";
+	["Zangarmarsh"] = "Zangarmarsh";
+	["Zoram'gar Outpost"] = "Zoram'gar Outpost";
+
+};

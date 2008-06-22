ef7d69baff1221087d322cd2bf858983b711a285
mr.kevin.fischer <mr.kevin.fischer@23c0fd96-f639-0410-9050-cdd92df7801c>
2008-06-22 17:05:58 +0000

    - frFR update

--- trunk/Atlas_OutdoorRaids/Locale/Atlas_OutdoorRaids-frFR.lua
+++ trunk/Atlas_OutdoorRaids/Locale/Atlas_OutdoorRaids-frFR.lua
@@ -23,104 +23,69 @@
 --]]
 
 if ( GetLocale() == "frFR" ) then
+AtlasORLocale = {
 
-	local BLUE = "|cff6666ff";
-	local GREY = "|cff999999";
-	local GREN = "|cff66cc33";
-	local _RED = "|cffcc6666";
-	local ORNG = "|cffcc9933";
-	local PURP = "|cff9900ff";
-	local INDENT = "      ";
+	["Ancient Skull Pile"] = "Ancienne pile de crânes";
+	["Ashenvale"] = "Orneval";
+	["Azshara"] = "Azshara";
+	["Azuregos"] = "Azuregos";
+	["Blackwind Landing"] = "Raie'odrome de Noirvent";
+	["Blackwind Valley"] = "Vallée de Noirvent";
+	["Blasted Lands"] = "Terres Foudroyées";
+	["Bough Shadow"] = "L'ombrage";
+	["Darkscreecher Akkarai"] = "Akkarai le Hurle-sombre";
+	["Doom Lord Kazzak"] = "Seigneur Funeste Kazzak";
+	["Doomwalker"] = "Marche-funeste";
+	["Dragons of Nightmare"] = "Dragons du Cauchemard";
+	["Dream Bough"] = "Bosquet du rêve";
+	["Dreamroarer"] = "Hurle-rêve";
+	["Dreamstalker"] = "Traque-rêve";
+	["Dreamtracker"] = "Cherche-rêve";
+	["Duskwood"] = "Bois de la Pénombre";
+	["Emeriss"] = "Emeriss";
+	["Feralas"] = "Féralas";
+	["Forge Camp: Mageddon"] = "Camp de Forge : Mageddon";
+	["Forge Camp: Rage"] = "Camp de Forge : Rage";
+	["Gezzarak the Huntress"] = "Gezzarak la Chasseresse";
+	["Graveyard"] = "Cimetière";
+	["Grella <Skyguard Quartermaster>"] = "Grella <Intendante de la Garde-ciel>";
+	["Hazzik"] = "Hazzik";
+	["Hazzik's Package"] = "Paquet d'Hazzik";
+	["Hellfire Peninsula"] = "Péninsule des Flammes Infernales";
+	["Highlord Kruul"] = "Généralissime Kruul";
+	["Invasion Point: Annihilator"] = "Site d'invasion : Annihilateur";
+	["Karrog"] = "Karrog";
+	["Lethlas"] = "Lethlas";
+	["Lethon"] = "Léthon";
+	["Nethergarde Keep"] = "Rempart-du-Néant";
+	["Outdoor Raid Encounters"] = "Rencontres de Raids extérieures";
+	["Phantim"] = "Phantim";
+	["Random"] = "Aléatoire";
+	["Rothos"] = "Rothos";
+	["Sahaak <Keeper of Scrolls>"] = "Sahaak <Gardiens des parchemins>";
+	["Seradane"] = "Seradane";
+	["Severin <Skyguard Medic>"] = "Séverin <Infirmier de la Garde-ciel>";
+	["Shadowmoon Valley"] = "Vallée d'Ombrelune";
+	["Skettis"] = "Skettis";
+	["Skull Pile"] = "Pile de crânes";
+	["Sky Commander Adaris"] = "Commandant Adaris de la Garde-ciel";
+	["Sky Sergeant Doryn"] = "Sergent Doryn de la Garde-ciel";
+	["Skyguard Handler Deesak"] = "Dresseur Deesak de la Garde-ciel";
+	["Skyguard Prisoner"] = "Prisonnier de la Garde-ciel";
+	["Summon"] = "Invoqué";
+	["Taerar"] = "Taerar";
+	["Talonpriest Ishaal"] = "Prêtre de la serre Ishaal";
+	["Talonpriest Skizzik"] = "Prêtre de la serre Skizzik";
+	["Talonpriest Zellek"] = "Prêtre de la serre Zellek";
+	["Terokk"] = "Terokk";
+	["Terokkar Forest"] = "Forêt de Terokkar";
+	["The Dragons"] = "Les Dragons";
+	["The Hinterlands"] = "Les Hinterlands";
+	["Thrallmar"] = "Thrallmar";
+	["Twilight Grove"] = "Le bosquet du crépuscule";
+	["Vakkiz the Windrager"] = "Vakkiz le Ragevent";
+	["Various"] = "Divers";
+	["Ysondre"] = "Ysondre";
 
-	local myCategory = "Rencontres de Raids extérieur";
-
-	local myData = {
-		Azuregos = {
-			ZoneName = "Azuregos";
-			Location = "Azshara";
-			GREY.."1) Azuregos";
-		};
-		FourDragons = {
-			ZoneName = "Les Dragons du Cauchemard";
-			Location = "Divers";
-			GREY.."1) Le bosquet du crépuscule, ".._RED.."Bois de la Pénombre";
-			GREY.."2) Seradane, ".._RED.."Les Hinterlands";
-			GREY..INDENT.."Rothos";
-			GREY..INDENT.."Cherche-rêve";
-			GREY.."3) Bosquet du rêve, ".._RED.."Féralas";
-			GREY..INDENT.."Lethlas";
-			GREY..INDENT.."Hurle-rêve";
-			GREY.."4) L'ombrage, ".._RED.."Orneval";
-			GREY..INDENT.."Phantim";
-			GREY..INDENT.."Traque-rêve";
-			"";
-			GREN.."Les Dragons";
-			GREY..INDENT.."Léthon";
-			GREY..INDENT.."Emeriss";
-			GREY..INDENT.."Taerar";
-			GREY..INDENT.."Ysondre";
-		};
-		HighlordKruul = {
-			ZoneName = "Généralissime Kruul";
-			Location = "Terres Foudroyées";
-			GREY.."1) Généralissime Kruul";
-			GREY.."2) Rempart-du-Néant";
-		};
-		DoomLordKazzak = {
-			ZoneName = "Seigneur Funeste Kazzak";
-			Location = "Péninsule des Flammes Infernales";
-			GREY.."1) Seigneur Funeste Kazzak";
-			GREY.."2) Site d'invasion : Annihilateur";
-			GREY.."3) Camp de Forge : Rage";
-			GREY.."4) Camp de Forge : Mageddon";
-			GREY.."5) Thrallmar";
-		};
-		Doomwalker = {
-			ZoneName = "Marche-funeste";
-			Location = "Vallée d'Ombrelune";
-			GREY.."1) Marche-funeste";
-		};
-		Skettis = {
-			ZoneName = "Skettis";
-			Location = "Vallée de Noirvent, Forêt de Terokkar";	
-			GREY.."1) Raie'odrome de Noirvent";
-			GREY..INDENT.."Commandant Adaris de la Garde-ciel";
-			GREY..INDENT.."Sergent Doryn de la Garde-ciel";
-			GREY..INDENT.."Dresseur Deesak de la Garde-ciel";
-			GREY..INDENT.."Séverin";
-			GREY..INDENT.."Grella";
-			GREY..INDENT.."Hazzik";
-			GREY.."2) Ancienne pile de crânes";
-			GREY..INDENT.."Terokk (Invoqué)";
-			GREY.."3) Sahaak";
-			GREY.."4) Prisonnier de la Garde-ciel (Hasard)";
-			GREY.."5) Prêtre de la serre Ishaal";
-			GREY.."6) Prêtre de la serre Skizzik";
-			GREY.."7) Prêtre de la serre Zellek";
-			GREY.."8) Paquet d'Hazzik";
-			GREY.."9) Cimetière";
-			GREN.."1') Pile de crânes";
-			GREN..INDENT.."Akkarai le Hurle-sombre (Invoqué)";
-			GREN..INDENT.."Gezzarak la Chasseresse (Invoqué)";
-			GREN..INDENT.."Karrog (Invoqué)";
-			GREN..INDENT.."Vakkiz le Ragevent (Invoqué)";
-		};		
-	};
-
-	myData.Azuregos.LevelRange =			"60+";
-	myData.FourDragons.LevelRange =			"60+";
-	myData.HighlordKruul.LevelRange =		"60+";
-	myData.DoomLordKazzak.LevelRange =		"70+";
-	myData.Doomwalker.LevelRange =			"70+";
-	myData.Skettis.LevelRange =				"70+";
-	
-	myData.Azuregos.PlayerLimit =			"40";
-	myData.FourDragons.PlayerLimit =		"40";
-	myData.HighlordKruul.PlayerLimit =		"40";
-	myData.DoomLordKazzak.PlayerLimit =		"40";
-	myData.Doomwalker.PlayerLimit =			"40";
-	myData.Skettis.PlayerLimit =			"40";
-
-	Atlas_RegisterPlugin("Atlas_OutdoorRaids", myCategory, myData);
-
+};
 end

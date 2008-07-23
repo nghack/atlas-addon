87b35801fa8494a336b8d2b9d43d9ff8c42c0634
StingerSoft@gmail.com <StingerSoft@gmail.com@23c0fd96-f639-0410-9050-cdd92df7801c>
2008-07-23 09:20:43 +0000

    Atlas_OutdoorRaids: added ruRU translation, not fully

---
+++ trunk/Atlas_OutdoorRaids/Locale/Atlas_OutdoorRaids-ruRU.lua
@@ -0,0 +1,92 @@
+﻿--[[
+
+	Atlas, a World of Warcraft instance map browser
+	Copyright 2005 - 2008 Dan Gilbert
+	Email me at loglow@gmail.com
+
+	This file is part of Atlas.
+
+	Atlas is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Atlas is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Atlas; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+--]]
+
+if ( GetLocale() == "ruRU" ) then
+
+AtlasORLocale = {
+
+	["Ancient Skull Pile"] = "Ancient Skull Pile";
+	["Ashenvale"] = "Ясеневый лес";
+	["Azshara"] = "Азшара";
+	["Azuregos"] = "Азурегос";
+	["Blackwind Landing"] = "Лагерь Черного Ветра";
+	["Blackwind Valley"] = "Долина Черного Ветра";
+	["Blasted Lands"] = "Выжженные земли";
+	["Bough Shadow"] = "Тенистая Крона";
+	["Darkscreecher Akkarai"] = "Темный визгун Аккарай";
+	["Doom Lord Kazzak"] = "Каззак Владыка Рока";
+	["Doomwalker"] = "Владыка Рока";
+	["Dragons of Nightmare"] = "Драконы Кошмаров";
+	["Dream Bough"] = "Сонные Ветви";
+	["Dreamroarer"] = "Dreamroarer";
+	["Dreamstalker"] = "Dreamstalker";
+	["Dreamtracker"] = "Сноходец";
+	["Duskwood"] = "Сумеречный лес";
+	["Emeriss"] = "Эмерисс";
+	["Feralas"] = "Фералас";
+	["Forge Camp: Mageddon"] = "Forge Camp: Mageddon";
+	["Forge Camp: Rage"] = "Лагерь Легиона: Ярость";
+	["Gezzarak the Huntress"] = "Геззарак Охотница";
+	["Graveyard"] = "Кладбище";
+	["Grella <Skyguard Quartermaster>"] = "Грелла <Интендант Стражи небес>";
+	["Hazzik"] = "Хаззик";
+	["Hazzik's Package"] = "Hazzik's Package";
+	["Hellfire Peninsula"] = "Полуостров Адского Пламени";
+	["Highlord Kruul"] = "Highlord Kruul";
+	["Invasion Point: Annihilator"] = "Invasion Point: Annihilator";
+	["Karrog"] = "Каррог";
+	["Lethlas"] = "Lethlas";
+	["Lethon"] = "Летон";
+	["Nethergarde Keep"] = "Крепость Стражей Пустоты";
+	["Outdoor Raid Encounters"] = "Места внешних рейдовых схваток";
+	["Phantim"] = "Phantim";
+	["Random"] = "Случайный";
+	["Rothos"] = "Ротос";
+	["Sahaak <Keeper of Scrolls>"] = "Сахаак <Хранитель Свитков>";
+	["Seradane"] = "Серадан";
+	["Severin <Skyguard Medic>"] = "Северин <Врач Стражи небес>";
+	["Shadowmoon Valley"] = "Долина Призрачной Луны";
+	["Skettis"] = "Скеттис";
+	["Skull Pile"] = "Skull Pile";
+	["Sky Commander Adaris"] = "Небесный Командир Адарис";
+	["Sky Sergeant Doryn"] = "Небесный сержант Дорин";
+	["Skyguard Handler Deesak"] = "Страж небес Дисак";
+	["Skyguard Prisoner"] = "Пленный страж небес";
+	["Summon"] = "Призыв";
+	["Taerar"] = "Таэрар";
+	["Talonpriest Ishaal"] = "Talonpriest Ishaal";
+	["Talonpriest Skizzik"] = "Talonpriest Skizzik";
+	["Talonpriest Zellek"] = "Talonpriest Zellek";
+	["Terokk"] = "Терокк";
+	["Terokkar Forest"] = "Лес Тероккар";
+	["The Dragons"] = "Драконы";
+	["The Hinterlands"] = "Внутренние земли";
+	["Thrallmar"] = "Траллмар";
+	["Twilight Grove"] = "Мглистая роща";
+	["Vakkiz the Windrager"] = "Ваккиз Ветроярость";
+	["Various"] = "Различное";
+	["Ysondre"] = "Исондра";
+
+};
+end

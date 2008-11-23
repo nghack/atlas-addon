a135de0a1d64f34b334d9a4bd08cfb2883a3d1f8
loglow@gmail.com <loglow@gmail.com@23c0fd96-f639-0410-9050-cdd92df7801c>
2008-11-23 18:34:52 +0000

    tagging v1.13.0 release

---
+++ tags/v1.13.0/trunk/Atlas_DungeonLocs/Locale/Atlas_DungeonLocs-ruRU.lua
@@ -0,0 +1,147 @@
+--[[
+
+-- Atlas Data  (Russian)
+-- Compiled by StingerSoft
+-- stingersoft@gmail.com
+-- Last Update: 27.09.2008
+
+--]]
+
+if ( GetLocale() == "ruRU" ) then
+
+AtlasDLLocale = {
+
+	--Common
+	["Battlegrounds"] = "Поля сражений";
+	["Blue"] = "Синий";
+	["Dungeon Locations"] = "Расположение подземелий";
+	["Instances"] = "Инстансы";
+	["White"] = "Белый";
+
+	--Places
+	["Alterac Mountains"] = "Альтеракские горы";
+	["Alterac Valley"] = "Альтеракская долина";
+	["Arathi Basin"] = "Низина Арати";
+	["Arathi Highlands"] = "Нагорье Арати";
+	["Ashenvale"] = "Ясеневый лес";
+	["Auchenai Crypts"] = "Аукенайские гробницы";
+	["Auchindoun"] = "Аукиндон";
+	["Badlands"] = "Бесплодные земли";
+	["Black Temple"] = "Черный храм";
+	["Blackfathom Deeps"] = "Непроглядная Пучина";
+	["Blackrock Depths"] = "Глубины Черной горы";
+	["Blackrock Mountain"] = "Черная гора";
+	["Blackrock Spire"] = "Вершина Черной горы";
+	["Blackwing Lair"] = "Логово Крыла Тьмы";
+	["Blade's Edge Mountains"] = "Острогорье";
+	["Caverns of Time"] = "Пещеры Времени";
+	["Coilfang Reservoir"] = "Резервуар Кривого Клыка";
+	["Deadwind Pass"] = "Перевал Мертвого Ветра";
+	["Desolace"] = "Пустоши";
+	["Dire Maul"] = "Забытый Город";
+	["Dun Morogh"] = "Дун Морог";
+	["Dustwallow Marsh"] = "Пылевые топи";
+	["Eastern Kingdoms"] = "Восточные королевства";
+	["Eastern Plaguelands"] = "Восточное Лихоземье";
+	["Feralas"] = "Фералас";
+	["Ghostlands"] = "Призрачные земли";
+	["Gnomeregan"] = "Гномреган";
+	["Gruul's Lair"] = "Логово Груула";
+	["Hellfire Citadel"] = "Цитадель Адского Пламени";
+	["Hellfire Peninsula"] = "Полуостров Адского Пламени";
+	["Hellfire Ramparts"] = "Бастионы Адского Пламени";
+	["Hillsbrad Foothills"] = "Предгорья Хилсбрада";
+	["Hyjal Summit"] = "Вершина Хиджала";
+	["Isle of Quel'Danas"] = "Остров Кель'Данас";
+	["Kalimdor"] = "Калимдор";
+	["Karazhan"] = "Каражан";
+	["Magisters' Terrace"] = "Терраса Магистров";
+	["Magtheridon's Lair"] = "Логово Магтеридона";
+	["Mana-Tombs"] = "Гробницы Маны";
+	["Maraudon"] = "Мародон";
+	["Molten Core"] = "Огненные Недра";
+	["Naxxramas"] = "Наксрамас";
+	["Netherstorm"] = "Пустоверть";
+	["Old Hillsbrad Foothills"] = "Старые предгорья Хилсбрада";
+	["Onyxia's Lair"] = "Логово Ониксии";
+	["Orgrimmar"] = "Оргриммар";
+	["Outland"] = "Запределье";
+	["Ragefire Chasm"] = "Огненная пропасть";
+	["Razorfen Downs"] = "Курганы Иглошкурых";
+	["Razorfen Kraul"] = "Лабиринты Иглошкурых";
+	["Ruins of Ahn'Qiraj"] = "Руины Ан'Киража";
+	["Scarlet Monastery"] = "Монастырь Алого ордена";
+	["Scholomance"] = "Некроситет";
+	["Serpentshrine Cavern"] = "Змеиное святилище";
+	["Sethekk Halls"] = "Сетеккские залы";
+	["Shadow Labyrinth"] = "Темный Лабиринт";
+	["Shadowfang Keep"] = "Крепость Темного Клыка";
+	["Shadowmoon Valley"] = "Долина Призрачной Луны";
+	["Silithus"] = "Силитус";
+	["Silverpine Forest"] = "Серебряный бор";
+	["Stormwind City"] = "Штормград";
+	["Stranglethorn Vale"] = "Тернистая долина";
+	["Stratholme"] = "Стратхольм";
+	["Sunken Temple"] = "Затонувший храм";
+	["Sunwell Plateau"] = "Плато Солнечного Колодца";
+	["Swamp of Sorrows"] = "Болото Печали";
+	["Tanaris"] = "Танарис";
+	["Tempest Keep"] = "Крепость Бурь";
+	["Temple of Ahn'Qiraj"] = "Храм Ан'Киража";
+	["Terokkar Forest"] = "Лес Тероккар";
+	["The Arcatraz"] = "Аркатрац";
+	["The Barrens"] = "Степи";
+	["The Black Morass"] = "Черные топи";
+	["The Blood Furnace"] = "Кузня Крови";
+	["The Botanica"] = "Ботаника";
+	["The Deadmines"] = "Мертвые копи";
+	["The Eye"] = "Око";
+	["The Mechanar"] = "Механар";
+	["The Nexus"] = "The Nexus";
+	["The Shattered Halls"] = "Разрушенные залы";
+	["The Slave Pens"] = "Узилище";
+	["The Steamvault"] = "Паровое Подземелье";
+	["The Stockade"] = "Тюрьма";
+	["The Underbog"] = "Нижетопь";
+	["Tirisfal Glades"] = "Тирисфальские леса";
+	["Uldaman"] = "Ульдаман";
+	["Wailing Caverns"] = "Пещеры Стенаний";
+	["Warsong Gulch"] = "Ущелье Песни Войны";
+	["Western Plaguelands"] = "Западное Лихоземье";
+	["Westfall"] = "Западный Край";
+	["Zangarmarsh"] = "Зангартопь";
+	["Zul'Aman"] = "Зул'Аман";
+	["Zul'Farrak"] = "Зул'Фаррак";
+	["Zul'Gurub"] = "Зул'Гуруб";
+
+	--untranslated
+	["Ahn'kahet: The Old Kingdom"] = "Ahn'kahet: The Old Kingdom";
+	["Azjol-Nerub"] = "Azjol-Nerub";
+	["Azjol-Nerub: The Upper Kingdom"] = "Azjol-Nerub: The Upper Kingdom";
+	["Coldarra"] = "Coldarra";
+	["Dalaran"] = "Dalaran";
+	["Dragonblight"] = "Dragonblight";
+	["Drak'Tharon Keep"] = "Drak'Tharon Keep";
+	["Grizzly Hills"] = "Grizzly Hills";
+	["Gundrak"] = "Gundrak";
+	["Halls of Lightning"] = "Halls of Lightning";
+	["Halls of Stone"] = "Halls of Stone";
+	["Howling Fjord"] = "Howling Fjord";
+	["Icecrown Citadel"] = "Icecrown Citadel";
+	["Icecrown"] = "Icecrown";
+	["Naxxramas"] = "Naxxramas";
+	["Northrend"] = "Northrend";
+	["Stratholme Past"] = "Stratholme Past";
+	["The Eye of Eternity"] = "The Eye of Eternity";
+	["The Nexus"] = "The Nexus";
+	["The Obsidian Sanctum"] = "The Obsidian Sanctum";
+	["The Oculus"] = "The Oculus";
+	["The Storm Peaks"] = "The Storm Peaks";
+	["The Violet Hold"] = "The Violet Hold";
+	["Ulduar"] = "Ulduar";
+	["Utgarde Keep"] = "Utgarde Keep";
+	["Utgarde Pinnacle"] = "Utgarde Pinnacle";
+	["Zul'Drak"] = "Zul'Drak";
+
+};
+end
---
+++ tags/v1.13.0/trunk/Atlas_Transportation/Locale/Atlas_Transportation-ruRU.lua
@@ -0,0 +1,260 @@
+--[[
+
+	Atlas, a World of Warcraft instance map browser
+	Copyright 2005 - 2008 Dan Gilbert
+	Email me at loglow@gmail.com
+
+	This file is part of Atlas.
+
+	Atlas is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Atlas is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Atlas; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+--]]
+
+--[[
+-- Atlas Localization Data (Russian)
+-- Translated by Nitrogen (Exorsus Guild)
+-- Свежеватель Душ
+-- Last Update : 11/4/2008
+--]]
+
+if ( GetLocale() == "ruRU" ) then
+
+AtlasTransLocale = {
+
+	["Aerie Peak"] = "Заоблачный Пик";
+	["Allerian Stronghold"] = "Бастион Аллерии";
+	["Alliance"] = "Альянс";
+	["Altar of Sha'tar"] = "Алтарь Ша'тар";
+	["Arathi Highlands"] = "Нагорье Арати";
+	["Area 52"] = "Зона 52";
+	["Ashenvale"] = "Ясеневый лес";
+	["Astranaar"] = "Астранаар";
+	["Auberdine"] = "Аубердин";
+	["Azshara"] = "Азшара";
+	["Azuremyst Isle"] = "Остров Лазурной Дымки";
+	["Badlands"] = "Бесплодные земли";
+	["Blackwind Landing"] = "Лагерь Черного Ветра";
+	["Blade's Edge Mountains"] = "Острогорье";
+	["Blasted Lands"] = "Выжженные земли";
+	["Blood Watch"] = "Кровавая Застава";
+	["Bloodmyst Isle"] = "Остров Кровавой Дымки";
+	["Bloodvenom Post"] = "Застава Отравленной Крови";
+	["Booty Bay"] = "Пиратская бухта";
+	["Brackenwall Village"] = "Деревня Гиблотопь";
+	["Burning Steppes"] = "Пылающие степи";
+	["Camp Mojache"] = "Лагерь Мохаче";
+	["Camp Taurajo"] = "Лагерь Таурахо";
+	["Cenarion Hold"] = "Крепость Кенария";
+	["Chillwind Point"] = "Лагерь Промозглого Ветра";
+	["Cosmowrench"] = "Космоворот";
+	["Crossroads"] = "Перекресток";
+	["Darkshire"] = "Темнолесье";
+	["Darkshore"] = "Темные берега";
+	["Desolace"] = "Пустоши";
+	["Druid-only"] = "Только-Друид";
+	["Dun Morogh"] = "Дун Морог";
+	["Durotar"] = "Степи";
+	["Duskwood"] = "Сумеречный лес";
+	["Dustwallow Marsh"] = "Пылевые топи";
+	["Eastern Kingdoms"] = "Восточные Королевства";
+	["Eastern Plaguelands"] = "Восточные Чумные земли";
+	["Elwynn Forest"] = "Элвиннский лес";
+	["Emerald Sanctuary"] = "Изумрудное святилище";
+	["Evergrove"] = "Вечная роща";
+	["Everlook"] = "Круговзор";
+	["Eversong Woods"] = "Леса Вечной Песни";
+	["Falcon Watch"] = "Соколиный Дозор";
+	["Feathermoon Stronghold"] = "Крепость Оперенной Луны";
+	["Felwood"] = "Оскверненный лес";
+	["Feralas"] = "Фералас";
+	["Flame Crest"] = "Пламенеющий Стяг";
+	["Forest Song"] = "Лесная Песнь";
+	["Freewind Post"] = "Застава Вольного Ветра";
+	["Gadgetzan"] = "Прибамбасск";
+	["Garadar"] = "Гарадар";
+	["Ghostlands"] = "Призрачные земли";
+	["Grom'Gol Base Camp"] = "Лагерь Гром'гол";
+	["Hammerfall"] = "Павший Молот";
+	["Hatchet Hills"] = "Перевал Амани";
+	["Hellfire Peninsula"] = "Полуостров Адского Пламени";
+	["Hillsbrad Foothills"] = "Предгорья Хилсбрада";
+	["Honor Hold"] = "Оплот Чести";
+	["Horde"] = "Орда";
+	["Ironforge"] = "Стальгорн";
+	["Isle of Quel'Danas"] = "Остров Кель'Данас";
+	["Kalimdor"] = "Калимдор";
+	["Kargath"] = "Каргат";
+	["Lakeshire"] = "Приозерье";
+	["Light's Hope Chapel"] = "Часовня Последней Надежды";
+	["Loch Modan"] = "Лок Модан";
+	["Marshal's Refuge"] = "Укрытие Маршалла";
+	["Menethil Harbor"] = "Гавань Менетил";
+	["Mok'Nathal Village"] = "Деревня Мок'Натал";
+	["Moonglade"] = "Лунная поляна";
+	["Morgan's Vigil"] = "Дозор Моргана";
+	["Mudsprocket"] = "Шестермуть";
+	["Mulgore"] = "Мулгор";
+	["Nagrand"] = "Награнд";
+	["Nethergarde Keep"] = "Крепость Стражей Пустоты";
+	["Netherstorm"] = "Пустоверть";
+	["Nighthaven"] = "Ночная Гавань";
+	["Nijel's Point"] = "Высота Найджела";
+	["Orebor Harborage"] = "Прибежище Оребор";
+	["Orgrimmar"] = "Оргриммар";
+	["Outland"] = "Запределье";
+	["Ratchet"] = "Кабестан";
+	["Rebel Camp"] = "Лагерь Повстанцев";
+	["Redridge Mountains"] = "Красногорье";
+	["Refuge Pointe"] = "Опорный пункт";
+	["Requires honored faction with Sha'tari Skyguard"] = "Требуется Уважение с фракцией Стражи Небес Ша'тар";
+	["Revantusk Village"] = "Деревня Сломанного Клыка";
+	["Rut'Theran Village"] = "Деревня Рут'Теран";
+	["Sanctum of the Stars"] = "Святилище Звезд";
+	["Searing Gorge"] = "Тлеющее ущелье";
+	["Sentinel Hill"] = "Сторожевой холм";
+	["Shadowmoon Valley"] = "Долина Призрачной Луны";
+	["Shadowmoon Village"] = "Деревня Призрачной Луны";
+	["Shadowprey Village"] = "Деревня Ночных Охотников";
+	["Shatter Point"] = "Парящая застава";
+	["Shattrath City"] = "Шаттрат";
+	["Silithus"] = "Силитус";
+	["Silvermoon"] = "Луносвет";
+	["Silverpine Forest"] = "Серебряный бор";
+	["Skyguard Outpost"] = "Застава Стражи Небес";
+	["South of the path along Lake Elune'ara"] = "Южный путь вдоль Озера Алуне'ара";
+	["Southshore"] = "Южнобережье";
+	["Spinebreaker Post"] = "Застава Хребтолома";
+	["Splintertree Post"] = "Застава Расщепленного Дерева";
+	["Stonard"] = "Каменор";
+	["Stonebreaker Hold"] = "Форт Камнеломов";
+	["Stonetalon Mountains"] = "Когтистые горы";
+	["Stonetalon Peak"] = "Пик Каменного Когтя";
+	["Stormwind City"] = "Штормград";
+	["Stranglethorn Vale"] = "Тернистая долина";
+	["Sun's Reach"] = "Солнечный Край";
+	["Sun Rock Retreat"] = "Приют у Солнечного Камня";
+	["Swamp of Sorrows"] = "Болото Печали";
+	["Swamprat Post"] = "Застава Болотной Крысы";
+	["Sylvanaar"] = "Сильванаар";
+	["Talonbranch Glade"] = "Поляна Когтистых Ветвей";
+	["Talrendis Point"] = "Застава Талрендис";
+	["Tanaris"] = "Танарис";
+	["Tarren Mill"] = "Мельница Таррен";
+	["Telaar"] = "Телаар";
+	["Teldrassil"] = "Тельдрассил";
+	["Telredor"] = "Телредор";
+	["Temple of Telhamat"] = "Храм Телхамата";
+	["Terokkar Forest"] = "Лес Тероккар";
+	["Thalanaar"] = "Таланаар";
+	["The Aldor"] = "Алдоры";
+	["The Barrens"] = "Степи";
+	["The Dark Portal"] = "Темный Портал";
+	["The Exodar"] = "Экзодар";
+	["The Hinterlands"] = "Внутренние земли";
+	["The Scryers"] = "Провидцы";
+	["The Sepulcher"] = "Гробница";
+	["The Stormspire"] = "Штормовая Вершина";
+	["Thelsamar"] = "Телсамар";
+	["Theramore Isle"] = "Остров Терамор";
+	["Thorium Point"] = "Лагерь Братства Тория";
+	["Thousand Needles"] = "Тысяча Игл";
+	["Thrallmar"] = "Траллмар";
+	["Thunder Bluff"] = "Громовой Утес";
+	["Thunderlord Stronghold"] = "Thunderlord Stronghold";
+	["Tirisfal Glades"] = "Трисфальские леса";
+	["Toshley's Station"] = "Станция Тошли";
+	["Tranquillien"] = "Транквиллион";
+	["Un'Goro Crater"] = "Кратер Ун'Горо";
+	["Undercity"] = "Подгород";
+	["Valormok"] = "Храбростан";
+	["West of the path to Timbermaw Hold"] = "Западный путь к Древобрюхи";
+	["Western Plaguelands"] = "Западные Чумные земли";
+	["Westfall"] = "Западный Край";
+	["Wetlands"] = "Болотина";
+	["Wildhammer Stronghold"] = "Цитадель Громового Молота";
+	["Winterspring"] = "Зимние Ключи";
+	["Zabra'jin"] = "Забра'джин";
+	["Zangarmarsh"] = "Зангартопь";
+	["Zoram'gar Outpost"] = "Форт Зорам'Гар";
+
+	--untranslated
+	["Agmar's Hammer"] = "Agmar's Hammer";
+	["Alliance"] = "Alliance";
+	["Amber Ledge"] = "Amber Ledge";
+	["Amberpine Lodge"] = "Amberpine Lodge";
+	["Apothecary Camp"] = "Apothecary Camp";
+	["Bor'Gorok Outpost"] = "Bor'Gorok Outpost";
+	["Borean Tundra"] = "Borean Tundra";
+	["Bouldercrag's Refuge"] = "Bouldercrag's Refuge";
+	["Brann Bronzebeard's Camp"] = "Brann Bronzebeard's Camp";
+	["Camp One'Qwah"] = "Camp One'Qwah";
+	["Camp Tunka'lo"] = "Camp Tunka'lo";
+	["Camp Winterhoof"] = "Camp Winterhoof";
+	["Coldarra"] = "Coldarra";
+	["Conquest Hold"] = "Conquest Hold";
+	["Crystalsong Forest"] = "Crystalsong Forest";
+	["Dalaran"] = "Dalaran";
+	["Darnassus"] = "Darnassus";
+	["Dragonblight"] = "Dragonblight";
+	["Ebon Watch"] = "Ebon Watch";
+	["Elwynn Forest"] = "Elwynn Forest";
+	["Fizzcrank Airstrip"] = "Fizzcrank Airstrip";
+	["Fordragon Keep"] = "Fordragon Keep";
+	["Fort Wildevar"] = "Fort Wildevar";
+	["Frosthold"] = "Frosthold";
+	["Grizzly Hills"] = "Grizzly Hills";
+	["Grom'arsh Crash-Site"] = "Grom'arsh Crash-Site";
+	["Howling Fjord"] = "Howling Fjord";
+	["K3"] = "K3";
+	["Kamagua"] = "Kamagua";
+	["Kor'Kron Vanguard"] = "Kor'Kron Vanguard";
+	["Light's Breach"] = "Light's Breach";
+	["Menethil Harbor"] = "Menethil Harbor";
+	["Moa'ki Harbor"] = "Moa'ki Harbor";
+	["Nesingwary Base Camp"] = "Nesingwary Base Camp";
+	["New Agamand"] = "New Agamand";
+	["Northrend"] = "Northrend";
+	["River's Heart"] = "River's Heart";
+	["Sholazar Basin"] = "Sholazar Basin";
+	["Silvermoon City"] = "Silvermoon City";
+	["Star's Rest"] = "Star's Rest";
+	["Storm Peaks"] = "Storm Peaks";
+	["Stormwind City"] = "Stormwind City";
+	["Sun's Reach Sanctum"] = "Sun's Reach Sanctum";
+	["Sunreaver's Command"] = "Sunreaver's Command";
+	["Taunka'le Village"] = "Taunka'le Village";
+	["The Argent Stand"] = "The Argent Stand";
+	["The Forgotten Coast"] = "The Forgotten Coast";
+	["Transitus Shield"] = "Transitus Shield";
+	["Transportation Maps"] = "Transportation Maps";
+	["Ulduar"] = "Ulduar";
+	["Unu'pe"] = "Unu'pe";
+	["Valaar's Berth"] = "Valaar's Berth";
+	["Valgarde"] = "Valgarde";
+	["Valiance Keep"] = "Valiance Keep";
+	["Vengeance Landing"] = "Vengeance Landing";
+	["Venomspite"] = "Venomspite";
+	["Warsong Hold"] = "Warsong Hold";
+	["Westfall Brigade Encampment"] = "Westfall Brigade Encampment";
+	["Westguard Keep"] = "Westguard Keep";
+	["Wetlands"] = "Wetlands";
+	["Windrunner's Overlook"] = "Windrunner's Overlook";
+	["Wintergarde Keep"] = "Wintergarde Keep";
+	["Wyrmrest Temple"] = "Wyrmrest Temple";
+	["Zim'Torga"] = "Zim'Torga";
+	["Zul'Drak"] = "Zul'Drak";
+
+};
+end

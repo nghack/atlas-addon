b7a1cf5bfd21b44553a49b24079d625582ea81b7
arithmandar <arithmandar@23c0fd96-f639-0410-9050-cdd92df7801c>
2010-11-29 07:34:49 +0000

    zhTW BattleGround update

--- trunk/Plugins/Atlas_Battlegrounds/Locale/Atlas_Battlegrounds-zhTW.lua
+++ trunk/Plugins/Atlas_Battlegrounds/Locale/Atlas_Battlegrounds-zhTW.lua
@@ -33,41 +33,41 @@ if AL then
 	AL["Battleground Maps"] = "戰場地圖";
 	AL["Entrance"] = "入口";
 	AL["North"] = "北";
-	AL["Orange"] = "橙";
-	AL["Red"] = "紅";
 	AL["Reputation"] = "聲望";
 	AL["Rescued"] = "營救";
 	AL["South"] = "南";
 	AL["Start"] = "開始";
 	AL["Summon"] = "召喚";
-	AL["White"] = "白";
 
 	--Places
 	AL["AV"] = "AV/奧山"; -- Alterac Valley
 	AL["AB"] = "AB/阿拉希"; -- Arathi Basin
 	AL["EotS"] = "EotS/暴風";
-	AL["IoC"] = "IoC"; -- Isle of Conquest
+	AL["IoC"] = "IoC"; -- Isle of Conquest 征服之島
 	AL["SotA"] = "SotA/遠祖"; -- Strand of the Ancients
 	AL["WSG"] = "WSG/戰歌"; -- Warsong Gulch
 
 	--Alterac Valley (North)
-	AL["Vanndar Stormpike <Stormpike General>"] = "范達爾·雷矛";
-	AL["Dun Baldar North Marshal"] = "丹巴達爾北部元帥";
-	AL["Dun Baldar South Marshal"] = "丹巴達爾南部元帥";
-	AL["Icewing Marshal"] = "冰翼元帥";
-	AL["Stonehearth Marshal"] = "石爐元帥";
+	AL["Vanndar Stormpike <Stormpike General>"] = "范達爾·雷矛 <雷矛將軍>";
 	AL["Prospector Stonehewer"] = "勘察員塔雷·石鎬";
-	AL["Morloch"] = "莫洛克";
-	AL["Umi Thorson"] = "烏米·托爾森";
-	AL["Keetar"] = "基塔爾";
-	AL["Arch Druid Renferal"] = "大德魯伊雷弗拉爾";
 	AL["Dun Baldar North Bunker"] = "丹巴達爾北部碉堡";
 	AL["Wing Commander Mulverick"] = "空軍指揮官穆維里克";--omitted from AVS
+	AL["Dun Baldar South Bunker"] = "丹巴達爾南部碉堡";
+	AL["Gaelden Hammersmith <Stormpike Supply Officer>"] = "蓋爾丁 <雷矛物資商人>";
+	AL["Stormpike Banner"] = "雷矛軍旗";
+	AL["Stormpike Lumber Yard"] = "雷矛林場";
+	AL["Wing Commander Jeztor"] = "空軍指揮官傑斯托";--omitted from AVS
+	AL["Wing Commander Guse"] = "空軍指揮官古斯";--omitted from AVS
+	AL["Captain Balinda Stonehearth <Stormpike Captain>"] = "巴琳達·石爐上尉 <雷矛上尉>";
+	AL["Western Crater"] = "西部凹地";
+	AL["Vipore's Beacon"] = "維波里的信號燈";
+	AL["Jeztor's Beacon"] = "傑斯托的信號燈";
+	AL["Eastern Crater"] = "東部凹地";
+	AL["Slidore's Beacon"] = "斯里多爾的信號燈";
+	AL["Guse's Beacon"] = "古斯的信號燈";
+	AL["Arch Druid Renferal"] = "大德魯伊雷弗拉爾";
 	AL["Murgot Deepforge"] = "莫高特·深爐";
-	AL["Dirk Swindle <Bounty Hunter>"] = "德爾克 <賞金獵人>";
-	AL["Athramanis <Bounty Hunter>"] = "亞斯拉瑪尼斯 <賞金獵人>";
 	AL["Lana Thunderbrew <Blacksmithing Supplies>"] = "蘭納·雷酒 <鐵匠供應商>";
-	AL["Stormpike Aid Station"] = "雷矛急救站";
 	AL["Stormpike Stable Master <Stable Master>"] = "雷矛獸欄管理員";
 	AL["Stormpike Ram Rider Commander"] = "雷矛山羊騎兵指揮官";
 	AL["Svalbrad Farmountain <Trade Goods>"] = "斯瓦爾布萊德·遠山 <商人>";
@@ -78,61 +78,70 @@ if AL then
 	AL["Wing Commander Ichman"] = "空軍指揮官艾克曼";--omitted from AVS
 	AL["Wing Commander Slidore"] = "空軍指揮官斯里多爾";--omitted from AVS
 	AL["Wing Commander Vipore"] = "空軍指揮官維波里";--omitted from AVS
-	AL["Dun Baldar South Bunker"] = "丹巴達爾南部碉堡";
-	AL["Corporal Noreg Stormpike"] = "諾雷格·雷矛下士";
-	AL["Gaelden Hammersmith <Stormpike Supply Officer>"] = "蓋爾丁 <雷矛物資商人>";
-	AL["Stormpike Banner"] = "雷矛軍旗";
-	AL["Stormpike Lumber Yard"] = "雷矛林場";
-	AL["Wing Commander Jeztor"] = "空軍指揮官傑斯托";--omitted from AVS
-	AL["Wing Commander Guse"] = "空軍指揮官古斯";--omitted from AVS
 	AL["Stormpike Ram Rider Commander"] = "雷矛山羊騎兵指揮官";
-	AL["Captain Balinda Stonehearth <Stormpike Captain>"] = "巴琳達·石爐上尉";
+	AL["Ivus the Forest Lord"] = "『森林之王』伊弗斯";
+	AL["Stormpike Aid Station"] = "雷矛急救站";
 	AL["Ichman's Beacon"] = "艾克曼的信號燈";
 	AL["Mulverick's Beacon"] = "穆維里克的信號燈";
-	AL["Ivus the Forest Lord"] = "『森林之王』伊弗斯";
-	AL["Western Crater"] = "西部凹地";
-	AL["Vipore's Beacon"] = "維波里的信號燈";
-	AL["Jeztor's Beacon"] = "傑斯托的信號燈";
-	AL["Eastern Crater"] = "東部凹地";
-	AL["Slidore's Beacon"] = "斯里多爾的信號燈";
-	AL["Guse's Beacon"] = "古斯的信號燈";
-	AL["Graveyards, Capturable Areas"] = "墓地, 可佔領的地區";--omitted from AVS
-	AL["Bunkers, Towers, Destroyable Areas"] = "碉堡, 哨塔, 可摧毀的地區";--omitted from AVS
-	AL["Assault NPCs, Quest Areas"] = "突擊 NPCs, 任務地區";--omitted from AVS
 
 	--Alterac Valley (South)
-	AL["Drek'Thar <Frostwolf General>"] = "德雷克塔爾";
-	AL["Duros"] = "杜洛斯";
-	AL["Drakan"] = "崔坎";
-	AL["West Frostwolf Warmaster"] = "西部霜狼將領";
-	AL["East Frostwolf Warmaster"] = "東部霜狼將領";
-	AL["Tower Point Warmaster"] = "哨塔高地將領";
-	AL["Iceblood Warmaster"] = "冰血將領";
-	AL["Lokholar the Ice Lord"] = "『冰雪之王』洛克霍拉";
-	AL["Captain Galvangar <Frostwolf Captain>"] = "加爾范上尉";
+	AL["Drek'Thar <Frostwolf General>"] = "德雷克塔爾 <霜狼將軍>";
+	AL["Captain Galvangar <Frostwolf Captain>"] = "加爾凡加上尉 <霜狼上尉>";
 	AL["Iceblood Tower"] = "冰血哨塔";
 	AL["Tower Point"] = "哨塔高地";
-	AL["Taskmaster Snivvle"] = "監工斯尼維爾";
-	AL["Masha Swiftcut"] = "瑪莎";
-	AL["Aggi Rumblestomp"] = "埃其";
+	AL["West Frostwolf Tower"] = "西部霜狼哨塔";
+	AL["East Frostwolf Tower"] = "東部霜狼哨塔";
+	AL["Frostwolf Banner"] = "霜狼軍旗";
+	AL["Lokholar the Ice Lord"] = "『冰雪之王』洛克霍拉";
 	AL["Jotek"] = "喬泰克";
 	AL["Smith Regzar"] = "鐵匠雷格薩";
 	AL["Primalist Thurloga"] = "原獵者瑟魯加";
-	AL["Sergeant Yazra Bloodsnarl"] = "亞斯拉·血矛";
 	AL["Frostwolf Stable Master <Stable Master>"] = "霜狼獸欄管理員";
 	AL["Frostwolf Wolf Rider Commander"] = "霜狼騎兵指揮官";
 	AL["Frostwolf Quartermaster"] = "霜狼軍需官";
-	AL["West Frostwolf Tower"] = "西部霜狼哨塔";
-	AL["East Frostwolf Tower"] = "東部霜狼哨塔";
 	AL["Frostwolf Relief Hut"] = "霜狼急救站";
-	AL["Frostwolf Banner"] = "霜狼軍旗";
 
 	--Arathi Basin
 
+	--Warsong Gulch
+
+	-- The Silithyst Must Flow
+	AL["The Silithyst Must Flow"] = "收集希利塞斯";
+	AL["Alliance's Camp"] = "聯盟營地";
+	AL["Horde's Camp"] = "部落營地";
+
 	--Eye of the Storm
 	AL["Flag"] = "旗幟";
+	AL["Graveyard"] = "墓地";
+
+	-- Halaa
+	AL["Quartermaster Davian Vaclav"] = "軍需官戴夫恩·瓦克拉夫";
+	AL["Chief Researcher Kartos"] = "首席調查員卡托斯";
+	AL["Aldraan <Blade Merchant>"] = "阿爾德蘭 <劍刃武器商>";
+	AL["Cendrii <Food & Drink>"] = "善德利 <食物和飲料>";
+	AL["Quartermaster Jaffrey Noreliqe"] = "軍需官傑夫利·諾利克";
+	AL["Chief Researcher Amereldine"] = "首席調查員阿莫瑞丹";
+	AL["Coreiel <Blade Merchant>"] = "寇瑞歐 <劍刃武器商>";
+	AL["Embelar <Food & Drink>"] = "安畢拉爾 <食物和飲料>";
+	AL["Wyvern Camp"] = "雙足翼龍營地";
+
+	-- Hellfire Peninsula PvP 
+	AL["Hellfire Fortifications"] = "地獄火防禦堡壘";
+
+	-- Terokkar Forest PvP
+	AL["Spirit Towers"] = "精神哨塔";
+
+	-- Zangarmarsh PvP
+	AL["West Beacon"] = "西部哨塔";
+	AL["East Beacon"] = "東部哨塔";
+	AL["Horde Field Scout"] = "部落戰場斥候";
+	AL["Alliance Field Scout"] = "聯盟戰場斥候";
+	AL["Twinspire Graveyard"] = "雙塔墓地";
 
 	--Isle of Conquest
+	AL["Gates are marked with red bars."] = "閘門以紅條標記.";
+	AL["Overlord Agmar"] = "霸主阿格瑪";
+	AL["High Commander Halford Wyrmbane <7th Legion>"] = "高階指揮官海弗德·龍禍 <第七軍團>";
 	AL["The Refinery"] = "精煉廠";
 	AL["The Docks"] = "碼頭";
 	AL["The Workshop"] = "工坊";
@@ -141,47 +150,16 @@ if AL then
 	AL["Contested Graveyards"] = "爭奪中的墓地";
 	AL["Horde Graveyard"] = "部落墓地";
 	AL["Alliance Graveyard"] = "聯盟墓地";
-	AL["Gates are marked with red bars."] = "閘門以紅條標記.";
-	AL["Overlord Agmar"] = "霸主阿格瑪";
-	AL["High Commander Halford Wyrmbane <7th Legion>"] = "高階指揮官海弗德·龍禍 <第七軍團>";
 
 	--Strand of the Ancients
+	AL["Gates are marked with their colors."] = "大門已被標記顏色";
 	AL["Attacking Team"] = "攻擊隊伍";
 	AL["Defending Team"] = "防禦隊伍";
 	AL["Massive Seaforium Charge"] = "巨型爆鹽炸藥";
-	AL["Battleground Demolisher"] = "戰場石毀車";
-	AL["Resurrection Point"] = "復活術點";
-	AL["Graveyard Flag"] = "墓地旗幟";
 	AL["Titan Relic"] = "泰坦聖物";
-	AL["Gates are marked with their colors."] = "大門已被標記顏色";
-
-	--Warsong Gulch
-
-	-- Hellfire Peninsula PvP 
-	AL["Hellfire Fortifications"] = "地獄火防禦堡壘";
-
-	-- Zangarmarsh PvP
-	AL["West Beacon"] = "西部哨塔";
-	AL["East Beacon"] = "東部哨塔";
-	AL["Twinspire Graveyard"] = "雙塔墓地";
-	AL["Alliance Field Scout"] = "聯盟戰場斥候";
-	AL["Horde Field Scout"] = "部落戰場斥候";
-
-	-- Terokkar Forest PvP
-	AL["Auchindoun Spirit Towers"] = "奧齊頓精神哨塔";
-
-	-- Halaa
-	AL["Wyvern Camp"] = "雙足翼龍營地";
-	AL["Quartermaster Jaffrey Noreliqe"] = "軍需官傑夫利·諾利克";
-	AL["Quartermaster Davian Vaclav"] = "軍需官戴夫恩·瓦克拉夫";
-	AL["Chief Researcher Amereldine"] = "首席調查員阿莫瑞丹";
-	AL["Chief Researcher Kartos"] = "首席調查員卡托斯";
-	AL["Aldraan <Blade Merchant>"] = "阿爾德蘭 <劍刃武器商>";
-	AL["Banro <Ammunition>"] = "班洛 <彈藥商>";
-	AL["Cendrii <Food & Drink>"] = "善德利 <食物和飲料>";
-	AL["Coreiel <Blade Merchant>"] = "寇瑞歐 <劍刃武器商>";
-	AL["Embelar <Food & Drink>"] = "安畢拉爾 <食物和飲料>";
-	AL["Tasaldan <Ammunition>"] = "塔薩丹 <彈藥商>";
+	AL["Battleground Demolisher"] = "戰場石毀車";
+	AL["Graveyard Flag"] = "墓地旗幟";
+	AL["Resurrection Point"] = "復活術點";
 
 	-- Wintergrasp
 	AL["Fortress Vihecal Workshop (E)"] = "堡壘載具工坊 (東)";
@@ -196,8 +174,20 @@ if AL then
 	AL["Southeast Graveyard"] = "東南墓地";
 	AL["Southwest Graveyard"] = "西南墓地";
 
-	-- Silithus - The Silithyst Must Flow
-	AL["The Silithyst Must Flow"] = "收集希利塞斯";
-	AL["Alliance's Camp"] = "聯盟營地";
-	AL["Horde's Camp"] = "部落營地";
+	-- The Battle for Gilneas
+	AL["Mines"] = "地雷";
+	AL["Lighthouse"] = "燈塔";
+--	AL["Waterworks"] = "供水設備";
+
+	-- Tol Barad
+	AL["Attackers"] = "攻擊者";
+	AL["Commander Stevens <Baradin's Wardens>"] = "Commander Stevens <巴拉丁鐵衛>";
+	AL["Sergeant Parker <Baradin's Wardens>"] = "Sergeant Parker <巴拉丁鐵衛>";
+	AL["Commander Zanoth <Hellscream's Reach>"] = "Commander Zanoth <地獄吼先鋒>";
+	AL["Drillmaster Razgoth <Hellscream's Reach>"] = "Drillmaster Razgoth <地獄吼先鋒>";
+	AL["Staff Sergeant Lazgar <Hellscream's Reach>"] = "Staff Sergeant Lazgar <地獄吼先鋒>";
+
+	-- Twin Peaks
+	AL["Wildhammer Longhouse"] = "Wildhammer Longhouse";
+	AL["Dragonmaw Clan Compound"] = "Dragonmaw Clan Compound";
 end

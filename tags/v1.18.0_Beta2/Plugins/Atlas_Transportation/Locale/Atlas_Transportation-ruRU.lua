ddbc6aed6a246ce5e86b01cd602557ed013dbd7c
arithmandar <arithmandar@23c0fd96-f639-0410-9050-cdd92df7801c>
2010-11-29 10:55:01 +0000

    Tag for v1.18.0 Beta 2

---
+++ tags/v1.18.0_Beta2/Plugins/Atlas_Transportation/Locale/Atlas_Transportation-ruRU.lua
@@ -0,0 +1,54 @@
+--[[
+
+	Atlas, a World of Warcraft instance map browser
+	Copyright 2005-2010 Dan Gilbert <dan.b.gilbert@gmail.com>
+	Copyright 2010 Lothaer <lothayer@gmail.com >, Atlas Team
+
+	This file is part of Atlas.
+
+	Atlas is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Atlas is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Atlas; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+--]]
+
+--[[
+-- Atlas Localization Data (Russian)
+-- Translated by Nitrogen (Exorsus Guild)
+-- Свежеватель Душ
+-- Last Update : 11/4/2008
+--]]
+
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local AL = AceLocale:NewLocale("Atlas_Transportation", "ruRU", false);
+-- Localize file must set above to false, for example:
+--    local AL = AceLocale:NewLocale("Atlas", "deDE", false);
+
+if AL then
+	AL["Death Knight Only"] = "Death Knight Only";		-- Need translation
+	AL["Druid-only"] = "Только-Друид";
+	AL["Legend"] = "Legend";				-- The chart's legend, for example, the purple line means the portal's path
+	AL["Orb of Translocation"] = "Orb of Translocation";	-- Need translation
+	AL["Portal Destinations"] = "Portal Destinations";	-- Need translation
+	AL["Portals"] = "Portals";				-- Need translation
+	AL["Portal / Waygate Path to the destination"] = "Portal / Waygate Path to the destination";
+	AL["Ship / Zeppelin sailing path to destination"] = "Ship / Zeppelin sailing path to destination";
+	AL["Requires honored faction with Sha'tari Skyguard"] = "Требуется Уважение с фракцией Стражи Небес Ша'тар";
+	AL["Seahorse"] = "Seahorse";				-- Need translation
+	AL["South of the path along Lake Elune'ara"] = "Южный путь вдоль Озера Алуне'ара";
+	AL["Taxi Nodes"] = "Taxi Nodes";			-- Need translation
+	AL["Transportation Maps"] = "Маршруты полётов";
+	AL["West of the path to Timbermaw Hold"] = "Западный путь в Крепость Древобрюхов";
+	AL["Zeppelin Towers"] = "Zeppelin Towers";		-- Need translation
+
+end

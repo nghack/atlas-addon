a7a6dce31450c143ba8c1511d214aa0e15e3f55c
arithmandar <arithmandar@23c0fd96-f639-0410-9050-cdd92df7801c>
2011-04-09 14:47:41 +0000

    Deprecated_List update

--- trunk/Core/Atlas.lua
+++ trunk/Core/Atlas.lua
@@ -736,7 +736,7 @@ local function Process_Deprecated()
 		
 		--most recent (working) versions of known modules at time of release
 		{ "AtlasWorld", "3.3.5.25" }, -- updated July 14, 2010
-		{ "AtlasQuest", "4.6.0" }, -- updated December 18, 2010
+		{ "AtlasQuest", "4.6.2" }, -- updated March 23, 2011
 		{ "AtlasMajorCities", "v1.5.3" }, -- updated November 15, 2010
 		{ "AtlasLoot", "6.02.00" }, -- updated January 30, 2011
 		{ "Atlas_Arena", "1.3" }, -- updated January, 10, 2011

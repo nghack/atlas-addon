cae6835462db331fab690c8f8aec71ed9513a15b
arithmandar <arithmandar@23c0fd96-f639-0410-9050-cdd92df7801c>
2011-06-16 15:38:18 +0000

    update Deprecated_List to revise AtlasLoot with version number 6.04.00

--- branches/v1.20.0/Core/Atlas.lua
+++ branches/v1.20.0/Core/Atlas.lua
@@ -544,7 +544,7 @@ local function Process_Deprecated()
 --		{ "AtlasWorld", "3.3.5.25" }, -- updated July 14, 2010 -- comment out because this plugin is no longer maintained
 		{ "AtlasQuest", "4.6.3" }, -- updated May 04, 2011
 --		{ "AtlasMajorCities", "v1.5.3" }, -- updated November 15, 2010; -- comment out because this plugin is no longer maintained
-		{ "AtlasLoot", "6.03.02" }, -- updated May 09, 2011
+		{ "AtlasLoot", "6.04.00" }, -- updated June XX, 2011
 		{ "Atlas_Arena", "1.3.2" }, -- updated April, 28, 2011
 		{ "Atlas_WorldEvents", "2.1" }, -- updated April 28, 2011
 	};

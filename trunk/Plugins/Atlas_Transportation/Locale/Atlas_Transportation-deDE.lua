51cb37833a78dea75569c8966c9fc3cbed0ff45a
dynaletik@googlemail.com <dynaletik@googlemail.com@23c0fd96-f639-0410-9050-cdd92df7801c>
2011-06-19 19:02:52 +0000

    - deDE update

--- trunk/Plugins/Atlas_Transportation/Locale/Atlas_Transportation-deDE.lua
+++ trunk/Plugins/Atlas_Transportation/Locale/Atlas_Transportation-deDE.lua
@@ -52,5 +52,8 @@ if AL then
 	AL["Wind Rider"] = "Windreiter";
 	AL["Won't be available once the Battle for Andorhal chain is finished."] = "Nicht mehr verfügbar, wenn die Schlacht um Andorhal abgeschlossen ist.";
 	AL["Zeppelin Towers"] = "Zeppelintürme";
+	
+	-- Mount Hyjal
+	AL["Nordune Ridge"] = "Nordunegrat";
 
 end

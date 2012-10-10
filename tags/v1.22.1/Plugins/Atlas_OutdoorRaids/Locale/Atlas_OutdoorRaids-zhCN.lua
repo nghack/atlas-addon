558ed9f7f94d0794e0f376dcfdc3a7a4d1845ac8
arithmandar <arithmandar@23c0fd96-f639-0410-9050-cdd92df7801c>
2012-10-10 16:16:13 +0000

    tag for v1.22.1 release

---
+++ tags/v1.22.1/Plugins/Atlas_OutdoorRaids/Locale/Atlas_OutdoorRaids-zhCN.lua
@@ -0,0 +1,60 @@
+﻿-- $Id$
+--[[
+
+	Atlas, a World of Warcraft instance map browser
+	Copyright 2005 ~ 2010 - Dan Gilbert <dan.b.gilbert@gmail.com>
+	Copyright 2010 - Lothaer <lothayer@gmail.com>, Atlas Team
+	Copyright 2011 ~ 2012 - Arith Hsu, Atlas Team <atlas.addon@gmail.com>
+
+	This file is part of Atlas.
+
+	Atlas is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Atlas is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Atlas; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+--]]
+-- Atlas Localization Data (Simplified Chinese)
+-- Initial translation by DiabloHu
+-- Maintained by DiabloHu, arith, Ananhaid
+
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local AL = AceLocale:NewLocale("Atlas_OutdoorRaids", "zhCN", false);
+-- Localize file must set above to false, for example:
+--    local AL = AceLocale:NewLocale("Atlas_OutdoorRaids", "deDE", false);
+
+if AL then
+	AL["Ancient Skull Pile"] = "上古颅骨堆";
+	AL["Darkscreecher Akkarai"] = "黑暗尖啸者阿克卡莱";
+	AL["Gezzarak the Huntress"] = "猎手吉萨拉克";
+	AL["Graveyard"] = "墓地";
+	AL["Grella <Skyguard Quartermaster>"] = "格蕾拉 <天空卫队军需官>";
+	AL["Hazzik"] = "哈吉克";
+	AL["Hazzik's Package"] = "哈吉克的包裹";
+	AL["Karrog"] = "卡尔洛格";
+	AL["Outdoor Raid Encounters"] = "户外首领分布图";
+	AL["Pandaria World Bosses"] = "潘达利亚世界首领";
+	AL["Random"] = "随机";
+	AL["Sahaak <Keeper of Scrolls>"] = "萨哈克 <卷轴保管者>";
+	AL["Severin <Skyguard Medic>"] = "瑟维里 <天空卫队医师>";
+	AL["Skull Pile"] = "颅骨堆";
+	AL["Sky Commander Adaris"] = "空军指挥官阿达利斯";
+	AL["Sky Sergeant Doryn"] = "空军上尉杜尔因";
+	AL["Skyguard Handler Deesak"] = "天空卫队训练师德萨克";
+	AL["Skyguard Prisoner"] = "天空卫队囚犯";
+	AL["Summon"] = "召唤";
+	AL["Talonpriest Ishaal"] = "鸦爪祭司伊沙尔";
+	AL["Talonpriest Skizzik"] = "鸦爪祭司斯卡基克";
+	AL["Talonpriest Zellek"] = "鸦爪祭司塞尔利克";
+	AL["Terokk"] = "泰罗克";
+	AL["Vakkiz the Windrager"] = "风怒者瓦克奇斯";
+end

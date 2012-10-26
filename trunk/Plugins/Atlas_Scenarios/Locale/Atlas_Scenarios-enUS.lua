7ae88f83b06f3cc804087ae347dfab9ef756031d
arithmandar <arithmandar@23c0fd96-f639-0410-9050-cdd92df7801c>
2012-10-26 18:33:31 +0000

    Maps' legend info update

--- trunk/Plugins/Atlas_Scenarios/Locale/Atlas_Scenarios-enUS.lua
+++ trunk/Plugins/Atlas_Scenarios/Locale/Atlas_Scenarios-enUS.lua
@@ -35,6 +35,7 @@ if AL then
 	AL["Stage 2"] = "Stage 2";
 	AL["Stage 3"] = "Stage 3";
 	AL["Stage 4"] = "Stage 4";
+	AL["Stage 5"] = "Stage 5";
 	AL["Final Stage"] = "Final Stage";
 	AL["Start"] = "Start";
 	AL["End"] = "End";
@@ -53,7 +54,79 @@ if AL then
 	AL["Defeat Jol'grum"] = "Defeat Jol'grum";
 	AL["Defeat Little Liuyang"] = "Defeat Little Liuyang";
 	AL["Defeat Chagan Firehoof"] = "Defeat Chagan Firehoof";
+	AL["Defeat the Final Challenger!"] = "Defeat the Final Challenger!";
 	AL["Defeat Maki Waterblade"] = "Defeat Maki Waterblade";
 	AL["Defeat Satay Byu"] = "Defeat Satay Byu";
 	AL["Defeat Cloudbender Kobo"] = "Defeat Cloudbender Kobo";
+
+	-- Assault on Zan'vess
+	
+	-- Theramore's Fall - Alliance
+	AL["To the Waterline!"] = "To the Waterline!";
+	AL["Rig powder barrels on all six ships."] = "Rig powder barrels on all six ships.";
+	AL["The Blastmaster"] = "The Blastmaster";
+	AL["Inform Blastmaster Sparkfuse of your success."] = "Inform Blastmaster Sparkfuse of your success.";
+	AL["Gryphon, Down"] = "Gryphon, Down";
+	AL["Slay Baldruc and his gryphons."] = "Slay Baldruc and his gryphons.";
+	AL["Tanks for Nothing!"] = "Tanks for Nothing!";
+	AL["Destroy Big Bessa and sabotage any unmanned tanks."] = "Destroy Big Bessa and sabotage any unmanned tanks.";
+	AL["Fly, Spy!"] = "Fly, Spy!";
+	AL["Slay Hedric Evencane and free Thalen Songweaver from Foothold Citadel."] = "Slay Hedric Evencane and free Thalen Songweaver from Foothold Citadel.";
+
+	-- Theramore's Fall - Horde
+	AL["Welcome to Theramore"] = "Welcome to Theramore";
+	AL["Defend the ship from oncoming Horde."] = "Defend the ship from oncoming Horde.";
+	AL["Sweep the Harbor"] = "Sweep the Harbor";
+	AL["Defeat the Horde in the harbor."] = "Defeat the Horde in the harbor.";
+	AL["Into the Ruins"] = "Into the Ruins";
+	AL["Fight past the enemy into the heart of ruined Theramore."] = "Fight past the enemy into the heart of ruined Theramore.";
+	AL["The Lady Proudmoore"] = "The Lady Proudmoore";
+	AL["Locate Jaina in the Mana Bomb crater."] = "Locate Jaina in the Mana Bomb crater.";
+	AL["Destroy the Destroyer"] = "Destroy the Destroyer";
+	AL["Destroy the siege engine at the west gate and recover the stolen standards."] = "Destroy the siege engine at the west gate and recover the stolen standards.";
+	AL["The Last Stand"] = "The Last Stand";
+	AL["Protect Jaina while she attunes to the Focusing Iris."] = "Protect Jaina while she attunes to the Focusing Iris.";
+
+	-- Unga Ingoo
+	AL["Escort Brewmaster Bo"] = "Escort Brewmaster Bo";
+	AL["Bring Brewmaster Bo to the Unga Ingoo Brewing Cauldron."] = "Bring Brewmaster Bo to the Unga Ingoo Brewing Cauldron.";
+	AL["Fill the Brewing Cauldron"] = "Fill the Brewing Cauldron";
+	AL["Bring 200 Unga Jungle Brew to the Brewing Cauldron."] = "Bring 200 Unga Jungle Brew to the Brewing Cauldron.";
+	AL["Defeat Captain Ook"] = "Defeat Captain Ook";
+	AL["Defeat Captain Ook."] = "Defeat Captain Ook.";
+
+	-- Brewmoon Festival
+	AL["The Brewmoon Festival!"] = "The Brewmoon Festival!";
+	AL["Help Brewmaster Boof's assistants gather the ingredients for the Kun-Lai Kicker."] = "Help Brewmaster Boof's assistants gather the ingredients for the Kun-Lai Kicker.";
+	AL["The Scouts Report."] = "The Scouts Report.";
+	AL["Investigate the Yaungol sightings."] = "Investigate the Yaungol sightings.";
+	AL["The Yaungol attack!"] = "The Yaungol attack!";
+	AL["Stop the Bataari invasion on Binan Village."] = "Stop the Bataari invasion on Binan Village.";
+	AL["Warbringer Qobi"] = "Warbringer Qobi";
+	AL["Defeat Warbringer Qobi."] = "Defeat Warbringer Qobi.";
+
+	-- Greenstone Village
+	AL["Rescue the Villagers"] = "Rescue the Villagers";
+	AL["Defeat the monsters attacking the Greenstone villagers."] = "Defeat the monsters attacking the Greenstone villagers.";
+	AL["Rescue the Dojo"] = "Rescue the Dojo";
+	AL["Defeat the monsters attacking Greenstone's dojo."] = "Defeat the monsters attacking Greenstone's dojo.";
+	AL["Recover Burgled Barrels"] = "Recover Burgled Barrels";
+	AL["Find burgled brew barrels in the forest and return them to the village dojo."] = "Find burgled brew barrels in the forest and return them to the village dojo.";
+	AL["Rescue Greenstone Masons"] = "Rescue Greenstone Masons";
+	AL["Travel to the Masons' Quarter and awaken the unconscious villagers."] = "Travel to the Masons' Quarter and awaken the unconscious villagers.";
+	AL["Defeat Vengeful Hui"] = "Defeat Vengeful Hui";
+	AL["Defeat Vengeful Hui."] = "Defeat Vengeful Hui.";
+
+	-- Crypt of Forgotten Kings
+	AL["End the Terror"] = "End the Terror";
+	AL["Defeat the corrupted Shado-Pan Leader."] = "Defeat the corrupted Shado-Pan Leader.";
+	AL["The Pool of Life"] = "The Pool of Life";
+	AL["Cleanse the Sha corruption from the Pool of Life."] = "Cleanse the Sha corruption from the Pool of Life.";
+	AL["Search the Crypts"] = "Search the Crypts";
+	AL["Delve deeper into the crypt and search the bottom level."] = "Delve deeper into the crypt and search the bottom level.";
+	AL["Destroy the Sha"] = "Destroy the Sha";
+	AL["Destroy the Abomination of Anger."] = "Destroy the Abomination of Anger.";
+
+	-- Peak of Serenity
+	
 end

95cac623ec9085003eb85ab472d7bb56d6208706
Dynaletik <Dynaletik@23c0fd96-f639-0410-9050-cdd92df7801c>
2013-05-21 19:13:46 +0000

    - fix the Isle of Thunder portal destinations

--- trunk/Plugins/Atlas_Transportation/Atlas_Transportation.lua
+++ trunk/Plugins/Atlas_Transportation/Atlas_Transportation.lua
@@ -865,7 +865,7 @@ local myData = {
 		ZoneName = { BZ["Pandaria"].." ("..FACTION_ALLIANCE..")" };
 		{ _RED..AL["Portals"] };
 		{ PURP.." A) "..BZ["Paw'Don Village"]..", "..BZ["The Jade Forest"] };
-		{ PURP.." B) "..BZ["The Seabolt"].." / "..BZ["Za'Tual"]..", "..BZ["Isle of Thunder"] };
+		{ PURP.." B) "..BZ["Violet Rise"]..", "..BZ["Isle of Thunder"] };
 		{ PURP.." A') "..BZ["Stormwind City"]..", "..BZ["Elwynn Forest"]..", "..BZ["Eastern Kingdoms"] };
 		{ PURP.." B') "..BZ["Ironforge"]..", "..BZ["Dun Morogh"]..", "..BZ["Eastern Kingdoms"] };
 		{ PURP.." C') "..BZ["Darnassus"]..", "..BZ["Teldrassil"]..", "..BZ["Kalimdor"] };
@@ -953,7 +953,7 @@ local myData = {
 		ZoneName = { BZ["Pandaria"].." ("..FACTION_HORDE..")" };
 		{ _RED..AL["Portals"] };
 		{ PURP.." A) "..BZ["Honeydew Village"]..", "..BZ["The Jade Forest"] };
-		{ PURP.." B) "..BZ["The Crimson Treader"].." / "..BZ["Court of Bones"]..", "..BZ["Isle of Thunder"] };
+		{ PURP.." B) "..BZ["Dawnseeker Promontory"]..", "..BZ["Isle of Thunder"] };
 		{ PURP.." A') "..BZ["Orgrimmar"]..", "..BZ["Durotar"]..", "..BZ["Kalimdor"] };
 		{ PURP.." B') "..BZ["Thunder Bluff"]..", "..BZ["Mulgore"]..", "..BZ["Kalimdor"] };
 		{ PURP.." C') "..BZ["Shattrath City"]..", "..BZ["Terokkar Forest"]..", "..BZ["Outland"] };
